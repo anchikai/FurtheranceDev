22f4721e1a00fe1d95a64847173c6bc59c90c013
anchikai <hentaisuperstar86@gmail.com>
2022-04-28 21:04:18 -0400

    Jar of Manna changes

--- lua/Items/JarOfManna.lua
+++ lua/Items/JarOfManna.lua
@@ -4,10 +4,21 @@ local game = Game()
 MannaOrb = Isaac.GetEntityVariantByName("Manna Orb")
 
 function mod:UseMannaJar(_, _, player)
-	local room = game:GetRoom()
-	local roomType = room:GetType()
-	local level = game:GetLevel()
-	
+	local data = mod:GetData(player)
+	data.MannaCount = 0
+	-- If the player has the same amount of coins bombs & keys
+	if (player:GetNumCoins() == player:GetNumBombs()) and (player:GetNumCoins() == player:GetNumKeys()) then
+		print("well what the fuck")
+	-- If the player has the least amount of coins
+	elseif (player:GetNumCoins() < player:GetNumBombs()) and (player:GetNumCoins() < player:GetNumKeys()) then
+		player:AddCoins(1)
+	-- If the player has the least amount of bombs, or if their coins & bombs = the same and are less then keys, or if their bombs & keys = the same and are less then coins
+	elseif ((player:GetNumBombs() < player:GetNumCoins()) and (player:GetNumBombs() < player:GetNumKeys())) or ((player:GetNumBombs() == player:GetNumCoins()) and (player:GetNumBombs() < player:GetNumKeys())) or ((player:GetNumBombs() == player:GetNumKeys()) and (player:GetNumBombs() < player:GetNumCoins())) then
+		player:AddBombs(1)
+	-- If the player has the least amount of keys, or if their coins & keys = the same and are less then bombs
+	elseif ((player:GetNumKeys() < player:GetNumCoins()) and (player:GetNumKeys() < player:GetNumBombs())) or ((player:GetNumKeys() == player:GetNumCoins()) and (player:GetNumKeys() < player:GetNumBombs())) then
+		player:AddKeys(1)
+	end
 	return true
 end
 
@@ -18,11 +29,25 @@ function mod:SpawnMana(entity)
 		local player = game:GetPlayer(i)
 		local data = mod:GetData(player)
 		if player:HasCollectible(CollectibleType.COLLECTIBLE_JAR_OF_MANNA) then
-			manna = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.ENEMY_SOUL, 2, entity.Position, Vector.Zero, player):ToEffect()
+			--[[manna = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.ENEMY_SOUL, 2, entity.Position, Vector.Zero, player):ToEffect()
 			manna.Timeout = 1
 			manna.LifeSpan = 1
-			data.MannaTimer = 60
-			print("fart")
+			data.MannaTimer = 60]]
+			if data.MannaCount < 21 then
+				data.MannaCount = data.MannaCount + 1
+			end
+			if data.MannaCount == 20 then
+				if player:GetActiveItem(ActiveSlot.SLOT_PRIMARY) == CollectibleType.COLLECTIBLE_JAR_OF_MANNA then
+					player:SetActiveCharge(1, ActiveSlot.SLOT_PRIMARY)
+					game:GetHUD():FlashChargeBar(player, ActiveSlot.SLOT_PRIMARY)
+				elseif player:GetActiveItem(ActiveSlot.SLOT_SECONDARY) == CollectibleType.COLLECTIBLE_JAR_OF_MANNA then
+					player:SetActiveCharge(1, ActiveSlot.SLOT_SECONDARY)
+					game:GetHUD():FlashChargeBar(player, ActiveSlot.SLOT_SECONDARY)
+				end
+				SFXManager():Play(SoundEffect.SOUND_BATTERYCHARGE)
+				SFXManager():Play(SoundEffect.SOUND_ITEMRECHARGE)
+			end
+			print(data.MannaCount)
 		end
 	end
 end
