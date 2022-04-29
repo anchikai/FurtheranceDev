6087f435e270f1a7f06672dce8186766d5f27815
anchikai <hentaisuperstar86@gmail.com>
2022-04-28 22:11:41 -0400

    Juno error spam fix

--- lua/Items/Juno.lua
+++ lua/Items/Juno.lua
@@ -3,9 +3,32 @@ local game = Game()
 local rng = RNG()
 
 function mod:JunoTears(tear, collider)
-	local player = tear.Parent:ToPlayer()
-	if player:HasCollectible(CollectibleType.COLLECTIBLE_JUNO) then
-		if (collider:IsEnemy() and collider:IsVulnerableEnemy() and collider:IsActiveEnemy()) then
+	for i = 0, game:GetNumPlayers() - 1 do
+		local player = game:GetPlayer(i)
+		if player:HasCollectible(CollectibleType.COLLECTIBLE_JUNO) then
+			if (collider:IsEnemy() and collider:IsVulnerableEnemy() and collider:IsActiveEnemy()) then
+				local rollJuno = rng:RandomInt(100)
+				local data = mod:GetData(player)
+				if player.Luck > 11 then
+					if rng:RandomInt(4) == 1 and data.JunoTimer == 0 then
+						player:UseActiveItem(CollectibleType.COLLECTIBLE_ANIMA_SOLA, UseFlag.USE_NOANIM, -1)
+						data.JunoTimer = 300
+					end
+				elseif rollJuno <= (player.Luck*2+2) and data.JunoTimer == 0 then
+					player:UseActiveItem(CollectibleType.COLLECTIBLE_ANIMA_SOLA, UseFlag.USE_NOANIM, -1)
+					data.JunoTimer = 300
+				end
+			end
+		end
+	end
+end
+
+mod:AddCallback(ModCallbacks.MC_PRE_TEAR_COLLISION, mod.JunoTears)				-- Tears
+mod:AddCallback(ModCallbacks.MC_PRE_KNIFE_COLLISION, mod.JunoTears)				-- Mom's Knife
+mod:AddCallback(ModCallbacks.MC_POST_LASER_INIT, function(tear)					-- Brimstone and other lasers
+	for i = 0, game:GetNumPlayers() - 1 do
+		local player = game:GetPlayer(i)
+		if player:HasCollectible(CollectibleType.COLLECTIBLE_JUNO) then
 			local rollJuno = rng:RandomInt(100)
 			local data = mod:GetData(player)
 			if player.Luck > 11 then
@@ -17,25 +40,6 @@ function mod:JunoTears(tear, collider)
 				player:UseActiveItem(CollectibleType.COLLECTIBLE_ANIMA_SOLA, UseFlag.USE_NOANIM, -1)
 				data.JunoTimer = 300
 			end
-		end
-	end
-end
-
-mod:AddCallback(ModCallbacks.MC_PRE_TEAR_COLLISION, mod.JunoTears)				-- Tears
-mod:AddCallback(ModCallbacks.MC_PRE_KNIFE_COLLISION, mod.JunoTears)				-- Mom's Knife
-mod:AddCallback(ModCallbacks.MC_POST_LASER_INIT, function(tear)					-- Brimstone and other lasers
-	local player = tear.Parent:ToPlayer()
-	if player:HasCollectible(CollectibleType.COLLECTIBLE_JUNO) then
-		local rollJuno = rng:RandomInt(100)
-		local data = mod:GetData(player)
-		if player.Luck > 11 then
-			if rng:RandomInt(4) == 1 and data.JunoTimer == 0 then
-				player:UseActiveItem(CollectibleType.COLLECTIBLE_ANIMA_SOLA, UseFlag.USE_NOANIM, -1)
-				data.JunoTimer = 300
-			end
-		elseif rollJuno <= (player.Luck*2+2) and data.JunoTimer == 0 then
-			player:UseActiveItem(CollectibleType.COLLECTIBLE_ANIMA_SOLA, UseFlag.USE_NOANIM, -1)
-			data.JunoTimer = 300
 		end
 	end
 end)
