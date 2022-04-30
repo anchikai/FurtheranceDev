92ea7468a57f3453e6e8f153821b64a2dea38730
goldenstein64 <jamesramsauer@gmail.com>
2022-04-30 03:35:11 -0400

    Edited Apocalypse

--- lua/Items/Apocalypse.lua
+++ lua/Items/Apocalypse.lua
@@ -3,24 +3,19 @@ local game = Game()
 
 function mod:UseApocalypse(_, _, player)
     local data = player:GetData()
-    player:RemoveCollectible(CollectibleType.COLLECTIBLE_APOCALYPSE)
     if player:GetCollectibleCount() > 0 then
-        data.ApocalypseItemCount = 0
         data.ApocalypseItem = 0
-        repeat
-            if player:HasCollectible(data.ApocalypseItemCount) then
-                repeat
-                    player:RemoveCollectible(data.ApocalypseItemCount, false, ActiveSlot.SLOT_PRIMARY, true)
-                until
-                    player:HasCollectible(data.ApocalypseItemCount) == false
+        for i = 1, CollectibleType.NUM_COLLECTIBLES do
+            while player:HasCollectible(i) do
+                player:RemoveCollectible(i, false, ActiveSlot.SLOT_PRIMARY, true)
                 data.ApocalypseItem = data.ApocalypseItem + 1
             end
-            data.ApocalypseItemCount = data.ApocalypseItemCount + 1
-        until data.ApocalypseItemCount == Isaac.GetItemConfig():GetCollectibles().Size -1
-            print("Removed All Items")
-            print(data.ApocalypseItem)
         end
-	return true
+        print("Removed All Items")
+        print(data.ApocalypseItem)
+    end
+
+    return true
 end
 
 mod:AddCallback(ModCallbacks.MC_USE_ITEM, mod.UseApocalypse, CollectibleType.COLLECTIBLE_APOCALYPSE)
