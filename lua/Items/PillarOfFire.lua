146573bfdec62a599695242d099292e02a177aa2
anchikai <hentaisuperstar86@gmail.com>
2022-04-29 23:22:16 -0400

    Basic Pillar of Fire functionality

--- lua/Items/PillarOfFire.lua
+++ lua/Items/PillarOfFire.lua
@@ -1,2 +1,31 @@
 local mod = Furtherance
 local game = Game()
+
+function mod:SpawnFire(entity, damage, flag, source, frames)
+    if entity.Type == EntityType.ENTITY_PLAYER then
+        local player = entity:ToPlayer()
+        if (player and player:HasCollectible(CollectibleType.COLLECTIBLE_PILLAR_OF_FIRE)) then
+            local rng = player:GetCollectibleRNG(CollectibleType.COLLECTIBLE_PILLAR_OF_FIRE)
+            local rollFire = rng:RandomInt(100)+1
+			if rollFire <= (player.Luck*5)+5 then
+                local speed = 4
+				for i = 1, 5 do
+                    if rng:RandomInt(2) == 0 then
+                        RandomVector1 = rng:RandomFloat()*speed
+                    else
+                        RandomVector1 = -rng:RandomFloat()*speed
+                    end
+                    if rng:RandomInt(2) == 0 then
+                        RandomVector2 = rng:RandomFloat()*speed
+                    else
+                        RandomVector2 = -rng:RandomFloat()*speed
+                    end
+                    local fire = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.HOT_BOMB_FIRE, 0, player.Position, Vector(RandomVector1, RandomVector2), player):ToEffect()
+                    fire.Scale = fire.Scale / 1.5
+                end
+			end
+        end
+    end
+end
+
+mod:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, mod.SpawnFire, EntityType.ENTITY_PLAYER)
