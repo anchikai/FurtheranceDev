5d227885a631260a5c9ecb717b5f484b02836f82
anchikai <hentaisuperstar86@gmail.com>
2022-04-30 03:28:07 -0400

    Apocalypse changes

--- lua/Items/Apocalypse.lua
+++ lua/Items/Apocalypse.lua
@@ -5,19 +5,23 @@ function mod:UseApocalypse(_, _, player)
     local data = player:GetData()
     player:RemoveCollectible(CollectibleType.COLLECTIBLE_APOCALYPSE)
     if player:GetCollectibleCount() > 0 then
+        data.ApocalypseItemCount = 0
+        data.ApocalypseItem = 0
         repeat
-            data.items = player:GetCollectibleCount()
-        until data.items == 0
-            Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, 0, Vector.Zero, Vector.Zero, nil)
-            data.items = data.items - 1
+            if player:HasCollectible(data.ApocalypseItemCount) then
+                repeat
+                    player:RemoveCollectible(data.ApocalypseItemCount, false, ActiveSlot.SLOT_PRIMARY, true)
+                until
+                    player:HasCollectible(data.ApocalypseItemCount) == false
+                end
+                data.ApocalypseItem = data.ApocalypseItem + 1
+            end
+            data.ApocalypseItemCount = data.ApocalypseItemCount + 1
+        until data.ApocalypseItemCount == Isaac.GetItemConfig():GetCollectibles().Size -1
+            print("Removed All Items")
+            print(data.ApocalypseItem)
         end
 	return true
 end
 
 mod:AddCallback(ModCallbacks.MC_USE_ITEM, mod.UseApocalypse, CollectibleType.COLLECTIBLE_APOCALYPSE)
-
-function mod:ChargeApocalypse()
-
-end
-
-mod:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, mod.ChargeApocalypse)
--- lua/Items/SalineSpray.lua
+++ lua/Items/SalineSpray.lua
@@ -3,14 +3,14 @@ local game = Game()
 
 ---@param tear EntityTear
 function mod:FireSalineSprayTear(tear)
-  local player = tear.Parent:ToPlayer()
-  if player and player:HasTrinket(TrinketType.TRINKET_SALINE_SPRAY) then
-    local rng = player:GetTrinketRNG(TrinketType.TRINKET_SALINE_SPRAY)
-    if rng:RandomFloat() <= 0.05 then
-      tear.TearFlags = tear.TearFlags | TearFlags.TEAR_ICE
-      tear:ChangeVariant(TearVariant.ICE)
+    local player = tear.Parent:ToPlayer()
+    if player and player:HasTrinket(TrinketType.TRINKET_SALINE_SPRAY) then
+        local rng = player:GetTrinketRNG(TrinketType.TRINKET_SALINE_SPRAY)
+        if rng:RandomFloat() <= 0.05 then
+            tear.TearFlags = tear.TearFlags | TearFlags.TEAR_ICE
+            tear:ChangeVariant(TearVariant.ICE)
+        end
     end
-  end
 end
 
 mod:AddCallback(ModCallbacks.MC_POST_FIRE_TEAR, mod.FireSalineSprayTear)
