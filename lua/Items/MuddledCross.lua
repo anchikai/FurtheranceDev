3ad8854630e9e2409e763a0351af9e4ac85c2a33
anchikai <hentaisuperstar86@gmail.com>
2022-05-01 11:04:10 -0400

    Rewrote backdrop changing to be way more efficient

--- lua/Items/MuddledCross.lua
+++ lua/Items/MuddledCross.lua
@@ -1,121 +1,41 @@
 local mod = Furtherance
 local game = Game()
 local rng = RNG()
-
 mod.Flipped = false
+
+local function clamp(value, min, max)
+	return math.min(math.max(value, min), max)
+end
 
 local function switchBackground(isFlipped)
 	local level = game:GetLevel()
 	local room = game:GetRoom()
-
-	local levelName = level:GetName()
 	if isFlipped == true then
-		if room:GetType() == RoomType.ROOM_DEFAULT then
-			if levelName == "Basement I" or levelName == "Basement II" or levelName == "Basement XL" then -- Basement
-				game:ShowHallucination(0, BackdropType.CAVES)
-			elseif levelName == "Cellar I" or levelName == "Cellar II" or levelName == "Cellar XL" then -- Cellar
-				game:ShowHallucination(0, BackdropType.CATACOMBS)
-			elseif levelName == "Burning Basement I" or levelName == "Burning Basement II" or levelName == "Burning Basement XL" then -- Burning Basement
-				game:ShowHallucination(0, BackdropType.FLOODED_CAVES)
-			elseif levelName == "Caves I" or levelName == "Caves II" or levelName == "Caves XL" then -- Caves
-				game:ShowHallucination(0, BackdropType.DEPTHS)
-			elseif levelName == "Catacombs I" or levelName == "Catacombs II" or levelName == "Catacombs XL" then -- Catacombs
-				game:ShowHallucination(0, BackdropType.NECROPOLIS)
-			elseif levelName == "Flooded Caves I" or levelName == "Flooded Caves II" or levelName == "Flooded Caves XL" then -- Flooded Caves
-				game:ShowHallucination(0, BackdropType.DANK_DEPTHS)
-			elseif levelName == "Depths I" or levelName == "Depths II" or levelName == "Depths XL" then -- Depths
-				game:ShowHallucination(0, BackdropType.WOMB)
-			elseif levelName == "Necropolis I" or levelName == "Necropolis II" or levelName == "Necropolis XL" then -- Necropolis
-				game:ShowHallucination(0, BackdropType.UTERO)
-			elseif levelName == "Dank Depths I" or levelName == "Dank Depths II" or levelName == "Dank Depths XL" then -- Dank Depths
-				game:ShowHallucination(0, BackdropType.SCARRED_WOMB)
-			elseif levelName == "Womb I" or levelName == "Womb II" or levelName == "Womb XL" or levelName == "Utero I" or levelName == "Utero II" or levelName == "Utero XL" or levelName == "Scarred Womb I" or levelName == "Scarred Womb II" or levelName == "Scarred Womb XL" then -- Womb, Utero, Scarred Womb
-				game:ShowHallucination(0, BackdropType.BLUE_WOMB)
-			elseif levelName == "???" then -- Blue Womb
-				game:ShowHallucination(0, rng:RandomInt(2) + 14) -- Either pick Sheol or Cathedral
-			elseif levelName == "Sheol" then -- Sheol
-				game:ShowHallucination(0, BackdropType.DARKROOM)
-			elseif levelName == "Cathedral" then -- Cathedral
-				game:ShowHallucination(0, BackdropType.CHEST)
-			elseif levelName == "Dark Room" then -- Dark Room
-				game:ShowHallucination(0, BackdropType.DARKROOM)
-			elseif levelName == "Chest" then -- Chest
-				game:ShowHallucination(0, BackdropType.CHEST)
-			elseif levelName == "Downpour I" or levelName == "Downpour II" or levelName == "Downpour XL" then -- Downpour
-				game:ShowHallucination(0, BackdropType.MINES)
-			elseif levelName == "Mines I" or levelName == "Mines II" or levelName == "Mines XL" then -- Mines
-				game:ShowHallucination(0, BackdropType.MAUSOLEUM)
-			elseif levelName == "Mausoleum I" or levelName == "Mausoleum II" or levelName == "Mausoleum XL" then -- Mausoleum
-				game:ShowHallucination(0, BackdropType.CORPSE)
-			elseif levelName == "Corpse I" or levelName == "Corpse II" or levelName == "Corpse XL" then -- Corpse
-				game:ShowHallucination(0, BackdropType.CORPSE)
-			elseif levelName == "Dross I" or levelName == "Dross II" or levelName == "Dross II" then -- Dross
-				game:ShowHallucination(0, BackdropType.ASHPIT)
-			elseif levelName == "Ashpit I" or levelName == "Ashpit II" or levelName == "Ashpit XL" then -- Ass Shit
-				game:ShowHallucination(0, BackdropType.GEHENNA)
-			elseif levelName == "Gehenna I" or levelName == "Gehenna II" or levelName == "Gehenna XL" then -- Gehenna
-				game:ShowHallucination(0, BackdropType.MORTIS)
-			elseif levelName == "Mortis I" or levelName == "Mortis II" or levelName == "Mortis XL" then -- Mortis
-				game:ShowHallucination(0, BackdropType.MORTIS)
+		backdrop = room:GetBackdropType()
+		if room:GetType() == RoomType.ROOM_DEFAULT or room:GetType() == RoomType.ROOM_TREASURE then
+			if level:GetStageType() <= StageType.STAGETYPE_AFTERBIRTH then
+				if level:GetStage() < LevelStage.STAGE4_3 then
+					game:ShowHallucination(0, backdrop+3)
+				elseif level:GetStage() ~= LevelStage.STAGE4_3 and level:GetStage() < LevelStage.STAGE6 then
+					game:ShowHallucination(0, backdrop+2)
+				end
+			elseif level:GetStageType() >= StageType.STAGETYPE_REPENTANCE then
+				if level:GetStage() < LevelStage.STAGE4_1 then
+					if backdrop == clamp(backdrop, BackdropType.MAUSOLEUM2, BackdropType.MAUSOLEUM4) or backdrop == BackdropType.MAUSOLEUM then
+						game:ShowHallucination(0, BackdropType.CORPSE)
+					else
+						game:ShowHallucination(0, backdrop+1)
+					end
+				end
 			end
 		end
 	elseif isFlipped == false then
-		if levelName == "Basement I" or levelName == "Basement II" or levelName == "Basement XL" then
-			game:ShowHallucination(0, BackdropType.BASEMENT)
-		elseif levelName == "Cellar I" or levelName == "Cellar II" or levelName == "Cellar XL" then
-			game:ShowHallucination(0, BackdropType.CELLAR)
-		elseif levelName == "Burning Basement I" or levelName == "Burning Basement II" or levelName == "Burning Basement XL" then
-			game:ShowHallucination(0, BackdropType.BURNT_BASEMENT)
-		elseif levelName == "Caves I" or levelName == "Caves II" or levelName == "Caves XL" then
-			game:ShowHallucination(0, BackdropType.CAVES)
-		elseif levelName == "Catacombs I" or levelName == "Catacombs II" or levelName == "Catacombs XL" then
-			game:ShowHallucination(0, BackdropType.CATACOMBS)
-		elseif levelName == "Flooded Caves I" or levelName == "Flooded Caves II" or levelName == "Flooded Caves XL" then
-			game:ShowHallucination(0, BackdropType.FLOODED_CAVES)
-		elseif levelName == "Depths I" or levelName == "Depths II" or levelName == "Depths XL" then
-			game:ShowHallucination(0, BackdropType.DEPTHS)
-		elseif levelName == "Necropolis I" or levelName == "Necropolis II" or levelName == "Necropolis XL" then
-			game:ShowHallucination(0, BackdropType.NECROPOLIS)
-		elseif levelName == "Dank Depths I" or levelName == "Dank Depths II" or levelName == "Dank Depths XL" then
-			game:ShowHallucination(0, BackdropType.DANK_DEPTHS)
-		elseif levelName == "Womb I" or levelName == "Womb II" or levelName == "Womb XL" then
-			game:ShowHallucination(0, BackdropType.WOMB)
-		elseif levelName == "Utero I" or levelName == "Utero II" or levelName == "Utero XL" then
-			game:ShowHallucination(0, BackdropType.UTERO)
-		elseif levelName == "Scarred Womb I" or levelName == "Scarred Womb II" or levelName == "Scarred Womb XL" then
-			game:ShowHallucination(0, BackdropType.SCARRED_WOMB)
-		elseif levelName == "???" then
-			game:ShowHallucination(0, BackdropType.BLUE_WOMB)
-		elseif levelName == "Sheol" then
-			game:ShowHallucination(0, BackdropType.SHEOL)
-		elseif levelName == "Cathedral" then
-			game:ShowHallucination(0, BackdropType.CATHEDRAL)
-		elseif levelName == "Dark Room" then
-			game:ShowHallucination(0, BackdropType.DARKROOM)
-		elseif levelName == "Chest" then
-			game:ShowHallucination(0, BackdropType.CHEST)
-		elseif levelName == "Downpour I" or levelName == "Downpour II" or levelName == "Downpour XL" then
-			game:ShowHallucination(0, BackdropType.DOWNPOUR)
-		elseif levelName == "Mines I" or levelName == "Mines II" or levelName == "Mines XL" then
-			game:ShowHallucination(0, BackdropType.MINES)
-		elseif levelName == "Mausoleum I" or levelName == "Mausoleum II" or levelName == "Mausoleum XL" then
-			game:ShowHallucination(0, BackdropType.MAUSOLEUM)
-		elseif levelName == "Corpse I" or levelName == "Corpse II" or levelName == "Corpse XL" then
-			game:ShowHallucination(0, BackdropType.CORPSE)
-		elseif levelName == "Dross I" or levelName == "Dross II" or levelName == "Dross II" then
-			game:ShowHallucination(0, BackdropType.DROSS)
-		elseif levelName == "Ashpit I" or levelName == "Ashpit II" or levelName == "Ashpit XL" then
-			game:ShowHallucination(0, BackdropType.ASHPIT)
-		elseif levelName == "Gehenna I" or levelName == "Gehenna II" or levelName == "Gehenna XL" then
-			game:ShowHallucination(0, BackdropType.GEHENNA)
-		elseif levelName == "Mortis I" or levelName == "Mortis II" or levelName == "Mortis XL" then
-			game:ShowHallucination(0, BackdropType.MORTIS)
-		end
+		game:ShowHallucination(0, backdrop)
 	end
 	SFXManager():Stop(SoundEffect.SOUND_DEATH_CARD)
 end
 
-function mod:UseFlippedCross()
+function mod:UseFlippedCross(_, _, player)
 	local room = game:GetRoom()
 	game:ShakeScreen(10)
 
@@ -205,7 +125,6 @@ function mod:TougherEnemies(entity, damage, flags, source, frames)
 		local player = Isaac.GetPlayer(i)
 		local data = mod:GetData(player)
 		if entity:IsActiveEnemy(false) and entity:IsVulnerableEnemy() then
-			local rng = player:GetCollectibleRNG(CollectibleType.COLLECTIBLE_MUDDLED_CROSS)
 			if data.DamageTimeout == nil then
 				data.DamageTimeout = false
 			elseif data.DamageTimeout == true then
@@ -244,10 +163,6 @@ function mod:FixInputs(entity, hook, button)
 end
 
 mod:AddCallback(ModCallbacks.MC_INPUT_ACTION, mod.FixInputs, InputHook.GET_ACTION_VALUE)
-
-local function clamp(value, min, max)
-	return math.min(math.max(value, min), max)
-end
 
 local flipFactor = 0
 function mod:AnimateFlip()
