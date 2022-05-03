b20bffa139005930a347ffb27868fb4076875ce3
kittenchilly <mario.pikmin.zelda@gmail.com>
2022-05-03 09:59:03 -0500

    Overhaul shift key

--- lua/Items/Shift.lua
+++ lua/Items/Shift.lua
@@ -2,35 +2,28 @@ local mod = Furtherance
 local game = Game()
 
 function mod:UseShift(_, _, player)
-	local data = mod:GetData(player)
-	data.ShiftKeyPressed = true
-	data.ShiftMultiplier = 15
+	mod:GetData(player).ShiftMultiplier = 15
 	return true
 end
 mod:AddCallback(ModCallbacks.MC_USE_ITEM, mod.UseShift, CollectibleType.COLLECTIBLE_SHIFT_KEY)
 
-function mod:ShiftCache(player, flag)
-	local data = mod:GetData(player)
-	if data.ShiftMultiplier == nil or data.ShiftKeyPressed == false then
-		data.ShiftMultiplier = 0
-	end
-	if flag == CacheFlag.CACHE_DAMAGE then
-		player.Damage = player.Damage + data.ShiftMultiplier
+function mod:Shift_CacheEval(player, flag)
+	if player:GetEffects():GetCollectibleEffectNum(CollectibleType.COLLECTIBLE_SHIFT_KEY) > 0 then
+		if flag == CacheFlag.CACHE_DAMAGE then
+			player.Damage = player.Damage + mod:GetData(player).ShiftMultiplier
+		end
 	end
 end
-mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, mod.ShiftCache)
+mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, mod.Shift_CacheEval)
 
-function mod:ShiftTears(player)
-	local data = mod:GetData(player)
-	if data.ShiftKeyPressed == true then
-		if game:GetFrameCount()%15 == 0 then
+function mod:Shift_Peffect(player)
+	if player:GetEffects():GetCollectibleEffectNum(CollectibleType.COLLECTIBLE_SHIFT_KEY) > 0 then
+		if game:GetFrameCount() % 15 == 0 then
+			local data = mod:GetData(player)
 			data.ShiftMultiplier = data.ShiftMultiplier - 0.125
 			player:AddCacheFlags(CacheFlag.CACHE_DAMAGE)
 			player:EvaluateItems()
 		end
-		if data.ShiftMultiplier == 0 then
-			data.ShiftKeyPressed = false
-		end
 	end
 end
-mod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, mod.ShiftTears)
+mod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, mod.Shift_Peffect)
