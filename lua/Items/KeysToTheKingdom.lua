48bcdd5be650eec55c3fed5ef0815e4b1dd33112
PixelPlz <101890162+PixelPlz@users.noreply.github.com>
2022-05-05 20:26:31 +0200

    keys to the kingdom wip

--- lua/Items/KeysToTheKingdom.lua
+++ lua/Items/KeysToTheKingdom.lua
@@ -1,76 +1,167 @@
+--[[
+Keys to the Kingdom
+     Charge: Soul Charged like the Jar of Urns, 12 Charges
+     Flavor Text: “Divine Control + Strength in Salvation”
+     Item Quality: 4
+     Item Pool: Angel Room
+     Unlock Condition: Defeat Delirium as Peter
+
+Notes:
+When used, it has a variety of effects on Isaac's immediate surroundings (based on the room he's in).
+
+Whilst held, Isaac will receive an additional 12.5% chance toward angel/devil rooms, mirroring Bethany's Book of Virtues. (tho the api hates this so we might not get this one)
+
+When used in a room full of lesser enemies, they will all be spared instantly and clear the room, giving peter either a temporary floor-wide stat up or a small perma stat up
+
+In a boss room, the key fills the room with holy light and a spare timer akin to Plum's own ability to be spared will begin. Isaac must not damage the boss or the light will disappear and he must fight the boss as normal. Upon the timer ending, the light shrinks into a pillar in the center and absorbs the boss, "saving" their soul. Isaac receives a small assortment of permanent random stat increases (health excluded).
+When used in a "final boss" room, Isaac will only receive a single-use holy mantle effect, as those bosses cannot be saved. Mom is too far gone, Isaac is dead, and Satan is Satan (same logic applies to Moms's Heart/It Lives, ???, Lamb, Delirium, Mother, Ultra Greed(ier), Mother, Dogma, and the Beast).
+
+When used in a devil room, all items become free for you to pick only one as usual with zero penalty (if he can even get inside, as Bethany and Peter require some tricky maneuvering to squeeze in deals).
+
+X When used in an angel room, Isaac can summon a key piece so he doesn't have to challenge Gabriel and Uriel. Any further uses will give him an additional angel room item considering the luck required to get into one and the likely late game state he will be in to get to that point.
+]]--
+
+
+
+
 local mod = Furtherance
 local game = Game()
 
-function mod:UseKTTK(_, _, player)
+local KTTKinCleared = false
+local KTTKslot = ActiveSlot.SLOT_PRIMARY
+
+function mod:UseKTTK(_, _, player, _, slot, _)
 	local room = game:GetRoom()
 	local roomType = room:GetType()
 	local level = game:GetLevel()
-	local kttkRNG = player:GetCollectibleRNG(CollectibleType.COLLECTIBLE_KEYS_TO_THE_KINGDOM)
+	
+	-- Free Devil items
 	if roomType == RoomType.ROOM_DEVIL then
-		if kttkRNG:RandomInt(2) == 0 then
-			player:UseCard(Card.CARD_CREDIT, 257)
-		else
-			Isaac.Spawn(EntityType.ENTITY_FALLEN, 1, 0, room:GetCenterPos(), Vector.Zero, nil)
-		end
+		player:UseCard(Card.CARD_CREDIT, 257) -- replace with gay lost type of free items
+	
+	-- Get key piece / random item in Angel room
 	elseif roomType == RoomType.ROOM_ANGEL then
-		if player:HasCollectible(CollectibleType.COLLECTIBLE_KEY_PIECE_1) == false and player:HasCollectible(CollectibleType.COLLECTIBLE_KEY_PIECE_2) == false then
-			if kttkRNG:RandomInt(2) == 0 then
-				Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, CollectibleType.COLLECTIBLE_KEY_PIECE_1, Isaac.GetFreeNearPosition(room:GetCenterPos(), 0), Vector.Zero, player)
-			else
-				Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, CollectibleType.COLLECTIBLE_KEY_PIECE_2, Isaac.GetFreeNearPosition(room:GetCenterPos(), 0), Vector.Zero, player)
-			end
-		elseif player:HasCollectible(CollectibleType.COLLECTIBLE_KEY_PIECE_1) and player:HasCollectible(CollectibleType.COLLECTIBLE_KEY_PIECE_2) == false then
-			Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, CollectibleType.COLLECTIBLE_KEY_PIECE_2, Isaac.GetFreeNearPosition(room:GetCenterPos(), 0), Vector.Zero, player)
-		elseif player:HasCollectible(CollectibleType.COLLECTIBLE_KEY_PIECE_1) == false and player:HasCollectible(CollectibleType.COLLECTIBLE_KEY_PIECE_2) then
+		if not player:HasCollectible(CollectibleType.COLLECTIBLE_KEY_PIECE_1) then
 			Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, CollectibleType.COLLECTIBLE_KEY_PIECE_1, Isaac.GetFreeNearPosition(room:GetCenterPos(), 0), Vector.Zero, player)
-		elseif player:HasCollectible(CollectibleType.COLLECTIBLE_KEY_PIECE_1) and player:HasCollectible(CollectibleType.COLLECTIBLE_KEY_PIECE_2) then
+		elseif not player:HasCollectible(CollectibleType.COLLECTIBLE_KEY_PIECE_2) then
+			Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, CollectibleType.COLLECTIBLE_KEY_PIECE_2, Isaac.GetFreeNearPosition(room:GetCenterPos(), 0), Vector.Zero, player)
+		else -- Random item
 			Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, 0, Isaac.GetFreeNearPosition(room:GetCenterPos(), 0), Vector.Zero, player)
 		end
-	elseif room:IsClear() then
-		Isaac.Spawn(EntityType.ENTITY_PICKUP, 0, 0, Isaac.GetFreeNearPosition(room:GetCenterPos(), 0), Vector.Zero, player)
+	
+	-- Give the charge back if the room is cleared
+	elseif room:IsClear() and not (roomType == RoomType.ROOM_CHALLENGE) then
+		KTTKinCleared = true
+		KTTKslot = slot
+		return false
+	
+	-- Spare enemies in the room
 	else
-		player:UseActiveItem(CollectibleType.COLLECTIBLE_CRACK_THE_SKY)
+		for _,v in pairs(Isaac.GetRoomEntities()) do
+			if v:IsActiveEnemy(false) then
+				if v:IsBoss() then
+					-- boss
+					spareTimer = 30 * 30
+					
+				else
+					local glow = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.GROUND_GLOW, 0, v.Position, Vector.Zero, player):ToEffect()
+					glow:GetSprite().Scale = (v.Size / 15) * v.SizeMulti
+					glow:GetSprite().PlaybackSpeed = 0.1
+					local soul = Isaac.Spawn(EntityType.ENTITY_EFFECT, 7887, 1, v.Position, Vector.Zero, player):ToEffect()
+					soul:GetSprite():Play("Spared", true)
+					
+					SFXManager():Play(SoundEffect.SOUND_HOLY, 1.25)
+					v:Remove()
+					-- TODO: Give stats
+				end
+			end
+		end
 	end
+	
 	return true
 end
 mod:AddCallback(ModCallbacks.MC_USE_ITEM, mod.UseKTTK, CollectibleType.COLLECTIBLE_KEYS_TO_THE_KINGDOM)
 
+
+-- Give the charge back if the room is cleared
+function mod:KTTKrecharge(player, flag)
+	if KTTKinCleared then
+		KTTKinCleared = false
+		player:FullCharge(KTTKslot, true)
+		SFXManager():Stop(SoundEffect.SOUND_BATTERYCHARGE)
+		SFXManager():Stop(SoundEffect.SOUND_ITEMRECHARGE)
+	end
+end
+mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, mod.KTTKrecharge)
+
+
 function mod:kttkKills(entity)
 	for i = 0, game:GetNumPlayers() - 1 do
 		local player = game:GetPlayer(i)
+
 		if player:HasCollectible(CollectibleType.COLLECTIBLE_KEYS_TO_THE_KINGDOM) then
-			soul = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.ENEMY_SOUL, 0, entity.Position, Vector.Zero, player):ToEffect()
+			Isaac.Spawn(EntityType.ENTITY_EFFECT, 7887, 0, entity.Position, Vector.Zero, player):ToEffect()
 		end
 	end
 end
 mod:AddCallback(ModCallbacks.MC_POST_ENTITY_KILL, mod.kttkKills)
 
+
 function mod:EnemySouls(effect)
-	for i = 0, game:GetNumPlayers() - 1 do
-		local player = game:GetPlayer(i)
-		local data = mod:GetData(player)
-		local direction = (player.Position - soul.Position):Normalized()
-		soul.Velocity = direction * 12
-		if soul.Position:DistanceSquared(player.Position) < 400 then
-			data.Soul = true
-			soul:Remove()
+	if effect.SubType == 0 then
+		local sprite = effect:GetSprite()
+		if not sprite:IsPlaying("Move") then
+			sprite:Play("Move", true)
 		end
-		if data.Soul == true then
-			data.Soul = false
-			if player:GetActiveItem(ActiveSlot.SLOT_PRIMARY) == CollectibleType.COLLECTIBLE_KEYS_TO_THE_KINGDOM then
-				if player:GetActiveCharge(ActiveSlot.SLOT_PRIMARY) < 12 then
-					player:SetActiveCharge(player:GetActiveCharge(ActiveSlot.SLOT_PRIMARY)+1, ActiveSlot.SLOT_PRIMARY)
-					game:GetHUD():FlashChargeBar(player, ActiveSlot.SLOT_PRIMARY)
-					SFXManager():Play(SoundEffect.SOUND_BEEP)
-				else
-					game:GetHUD():FlashChargeBar(player, ActiveSlot.SLOT_PRIMARY)
-					SFXManager():Play(SoundEffect.SOUND_BATTERYCHARGE)
+		sprite.Offset = Vector(0,-14)
+	
+		for i = 0, game:GetNumPlayers() - 1 do
+			local player = game:GetPlayer(i)
+			local data = mod:GetData(player)
+			
+			effect.Velocity = (effect.Velocity + (((player.Position - effect.Position):Normalized() * 27) - effect.Velocity) * 0.45)
+			
+			sprite.Rotation = effect.Velocity:GetAngleDegrees() + 90
+
+			-- Collect soul
+			if effect.Position:DistanceSquared(player.Position) < 400 then
+				data.Soul = true
+				effect:Remove()
+			end
+			
+			-- Get charge from soul
+			-- TODO: don't get charges from room clears
+			if data.Soul == true then
+				local slot = nil
+				if player:GetActiveItem(ActiveSlot.SLOT_PRIMARY) == CollectibleType.COLLECTIBLE_KEYS_TO_THE_KINGDOM then
+					slot = ActiveSlot.SLOT_PRIMARY
+				elseif player:GetActiveItem(ActiveSlot.SLOT_SECONDARY) == CollectibleType.COLLECTIBLE_KEYS_TO_THE_KINGDOM then
+					slot = ActiveSlot.SLOT_SECONDARY
 				end
-			elseif player:GetActiveItem(ActiveSlot.SLOT_SECONDARY) == CollectibleType.COLLECTIBLE_KEYS_TO_THE_KINGDOM then
-				player:SetActiveCharge(player:GetActiveCharge(ActiveSlot.SLOT_SECONDARY)+1, ActiveSlot.SLOT_SECONDARY)
-				game:GetHUD():FlashChargeBar(player, ActiveSlot.SLOT_SECONDARY)
+				
+				if slot ~= nil then
+					if player:GetActiveCharge(slot) < 12 then
+						player:SetActiveCharge(player:GetActiveCharge(slot) + 1, slot)
+						game:GetHUD():FlashChargeBar(player, slot)
+						SFXManager():Play(SoundEffect.SOUND_BEEP)
+						
+						-- Play charged sound if soul charges it to max
+						if player:GetActiveCharge(slot) == 12 then
+							SFXManager():Play(SoundEffect.SOUND_BATTERYCHARGE)
+						end
+						
+					else
+						game:GetHUD():FlashChargeBar(player, slot)
+						SFXManager():Play(SoundEffect.SOUND_BATTERYCHARGE)
+					end
+					
+					player:SetColor(Color(1,1,1, 1, 0.25,0.25,0.25), 5, 1, true, false)
+					SFXManager():Play(SoundEffect.SOUND_SOUL_PICKUP)
+				end
+				
+				data.Soul = false
 			end
 		end
 	end
 end
-mod:AddCallback(ModCallbacks.MC_POST_EFFECT_UPDATE, mod.EnemySouls, EffectVariant.ENEMY_SOUL)
+mod:AddCallback(ModCallbacks.MC_POST_EFFECT_UPDATE, mod.EnemySouls, 7887)
