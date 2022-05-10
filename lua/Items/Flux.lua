6c6c304f3e216750c493dd24ebfa50c29fc21999
goldenstein64 <jamesramsauer@gmail.com>
2022-05-10 01:43:27 -0400

    Fixed Flux

--- lua/Items/Flux.lua
+++ lua/Items/Flux.lua
@@ -4,38 +4,22 @@ local game = Game()
 function mod:FluxTears(tear)
     local player = tear.SpawnerEntity:ToPlayer()
     if player and player:HasCollectible(CollectibleType.COLLECTIBLE_FLUX) then
-        local tearData = mod:GetData(tear)
         if tear.SubType == 0 and tear.FrameCount == 1 then
-            local fluxTear = player:FireTear(player.Position, Vector.Zero, true, false, true, player, 1)
-            tear.Velocity = Vector.Zero
+            local fluxTear = player:FireTear(player.Position, -tear.Velocity, true, false, true, player, 1)
             tear.SubType = 1
             fluxTear.SubType = 2
             fluxTear.Color = Color(0.75, 0, 0, 1, 0.25, 0, 0)
-
-            tearData.OriginalPosition = player.Position
-            tearData.Flux = fluxTear
         end
+    end
+
+    if tear.SubType == 1 then
+        tear.Velocity = player.Velocity * (2 + player.ShotSpeed)
+    elseif tear.SubType == 2 then
+        tear.Velocity = player.Velocity * (2 - player.ShotSpeed)
     end
 end
 
 mod:AddCallback(ModCallbacks.MC_POST_TEAR_UPDATE, mod.FluxTears)
-
-function mod:RenderFluxTears(tear)
-    local player = tear.SpawnerEntity:ToPlayer()
-
-    local tearData = mod:GetData(tear)
-
-    if tear.SubType ~= 1 then return end
-
-    local offset = (player.Position - tearData.OriginalPosition) * 1.2
-
-    tear.Velocity = player.Velocity * 1.2
-    tearData.Flux.Velocity = -player.Velocity * 1.2
-    tear.Position = tearData.OriginalPosition + offset
-    tearData.Flux.Position = tearData.OriginalPosition - offset
-end
-
-mod:AddCallback(ModCallbacks.MC_POST_TEAR_RENDER, mod.RenderFluxTears)
 
 function mod:FluxRange(player, flag)
     if player:HasCollectible(CollectibleType.COLLECTIBLE_FLUX) then
