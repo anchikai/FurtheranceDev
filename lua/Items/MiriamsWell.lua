d0009f6ec76d549e3370ad53f6091058848be542
anchikai <hentaisuperstar86@gmail.com>
2022-05-11 20:45:29 -0400

    Miriam's Well now has an animation
    Co-Authored-By: Demi <100974505+TheDemisemihemidemisemiquaver@users.noreply.github.com>

--- lua/Items/MiriamsWell.lua
+++ lua/Items/MiriamsWell.lua
@@ -17,28 +17,37 @@ mod:AddCallback(ModCallbacks.MC_FAMILIAR_INIT, mod.WellInit, MiriamWell)
 function mod:WellUpdate(Well)
     local data = mod:GetData(Well)
     local player = Well.SpawnerEntity:ToPlayer()
-    Well.OrbitDistance = Vector(45, 45)
+    Well.OrbitDistance = Vector(40, 40)
     Well.OrbitSpeed = 0.03
     Well.Velocity = Well:GetOrbitPosition(player.Position + player.Velocity) - Well.Position
+    local sprite = Well:GetSprite()
     if data.WellRespawnTimer > 0 then
         data.WellRespawnTimer = data.WellRespawnTimer - 1
         if data.WellRespawnTimer == 0 then
+            sprite:Load("gfx/miriams_well.anm2", true)
+            sprite:SetAnimation("Idle", true)
+            sprite:Play("Idle", true)
             data.WellRespawnTimer = -1
             data.WellDied = false
         end
+    end
+    if sprite:IsPlaying("Break") and sprite:GetFrame() == 10 then
+        local puddle = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.PLAYER_CREEP_HOLYWATER_TRAIL, 1, Well.Position, Vector.Zero, player):ToEffect()
+        puddle.CollisionDamage = player.Damage/2
+        SFXManager():Play(SoundEffect.SOUND_GASCAN_POUR)
     end
 end
 mod:AddCallback(ModCallbacks.MC_FAMILIAR_UPDATE, mod.WellUpdate, MiriamWell)
 
 function mod:WellCollide(Well, collider)
-    local player = Well.SpawnerEntity:ToPlayer()
     local data = mod:GetData(Well)
     if collider.Type ~= EntityType.ENTITY_PLAYER then
         if data.WellDied ~= true then
+            local sprite = Well:GetSprite()
+            sprite:Load("gfx/miriams_well.anm2", true)
+            sprite:SetAnimation("Break", true)
+            sprite:Play("Break", true)
             data.WellDied = true
-            SFXManager():Play(SoundEffect.SOUND_ROCK_CRUMBLE)
-            local puddle = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.PLAYER_CREEP_HOLYWATER_TRAIL, 1, Well.Position, Vector.Zero, player):ToEffect()
-            puddle.CollisionDamage = player.Damage/2
             data.WellRespawnTimer = 240
             if collider.Type == EntityType.ENTITY_PROJECTILE then
                 collider:Die()
@@ -56,19 +65,3 @@ function mod:MiriamCache(player, flag)
     end
 end
 mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, mod.MiriamCache)
-
-function mod:WellRender(Well)
-    local data = mod:GetData(Well)
-    local sprite = Well:GetSprite()
-    sprite:Load("gfx/miriams_well.anm2", true)
-    if data.WellDied then
-        if sprite:IsPlaying("Break") == false then
-            sprite:Play("Break", true)
-        end
-    else
-        if sprite:IsPlaying("Idle") == false then
-            sprite:Play("Idle", true)
-        end
-    end
-end
-mod:AddCallback(ModCallbacks.MC_POST_FAMILIAR_RENDER, mod.WellRender, MiriamWell)
