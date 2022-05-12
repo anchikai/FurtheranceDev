26d139791838fffe3b8f97203556e94d8901b6a6
anchikai <hentaisuperstar86@gmail.com>
2022-05-11 20:05:15 -0400

    Miriam's Well rewrite + New Sprites
    Co-Authored-By: goldenstein64 <goldnstein64@gmail.com>

--- lua/Items/MiriamsWell.lua
+++ lua/Items/MiriamsWell.lua
@@ -4,30 +4,47 @@ local game = Game()
 local MiriamWell = Isaac.GetEntityVariantByName("Miriam's Well")
 
 function mod:WellInit(Well)
+    local data = mod:GetData(Well)
+    data.WellRespawnTimer = -1
+    data.WellDied = false
     Well:AddToOrbit(1)
+    if data.WellRespawnTimer > 0 then
+        Well:Remove()
+    end
 end
 mod:AddCallback(ModCallbacks.MC_FAMILIAR_INIT, mod.WellInit, MiriamWell)
 
 function mod:WellUpdate(Well)
-    for i = 0, game:GetNumPlayers() - 1 do
-		local player = Isaac.GetPlayer(i)
-        Well.OrbitDistance = Vector(25, 25)
-        Well.OrbitSpeed = 0.045
-        Well.Velocity = Well:GetOrbitPosition(player.Position + player.Velocity) - Well.Position
+    local data = mod:GetData(Well)
+    local player = Well.SpawnerEntity:ToPlayer()
+    Well.OrbitDistance = Vector(45, 45)
+    Well.OrbitSpeed = 0.03
+    Well.Velocity = Well:GetOrbitPosition(player.Position + player.Velocity) - Well.Position
+    if data.WellRespawnTimer > 0 then
+        data.WellRespawnTimer = data.WellRespawnTimer - 1
+        if data.WellRespawnTimer == 0 then
+            data.WellRespawnTimer = -1
+            data.WellDied = false
+        end
     end
 end
 mod:AddCallback(ModCallbacks.MC_FAMILIAR_UPDATE, mod.WellUpdate, MiriamWell)
 
-RespawnTimer = -1
 function mod:WellCollide(Well, collider)
+    local player = Well.SpawnerEntity:ToPlayer()
+    local data = mod:GetData(Well)
     if collider.Type ~= EntityType.ENTITY_PLAYER then
-        SFXManager():Play(SoundEffect.SOUND_ROCK_CRUMBLE)
-        local puddle = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.PLAYER_CREEP_HOLYWATER_TRAIL, 1, Well.Position, Vector.Zero, player):ToEffect()
-        puddle.CollisionDamage = player.Damage/2
-        Well:Die()
-        RespawnTimer = 240
-        if collider.Type == EntityType.ENTITY_PROJECTILE then
-            collider:Die()
+        if data.WellDied ~= true then
+            data.WellDied = true
+            SFXManager():Play(SoundEffect.SOUND_ROCK_CRUMBLE)
+            local puddle = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.PLAYER_CREEP_HOLYWATER_TRAIL, 1, Well.Position, Vector.Zero, player):ToEffect()
+            puddle.CollisionDamage = player.Damage/2
+            data.WellRespawnTimer = 240
+            if collider.Type == EntityType.ENTITY_PROJECTILE then
+                collider:Die()
+            end
+        else
+            return true
         end
     end
 end
@@ -35,18 +52,23 @@ mod:AddCallback(ModCallbacks.MC_PRE_FAMILIAR_COLLISION, mod.WellCollide, MiriamW
 
 function mod:MiriamCache(player, flag)
     if flag == CacheFlag.CACHE_FAMILIARS then
-        player:CheckFamiliar(MiriamWell, player:GetCollectibleNum(CollectibleType.COLLECTIBLE_MIRIAMS_WELL), player:GetCollectibleRNG(CollectibleType.COLLECTIBLE_MIRIAMS_WELL))
+        player:CheckFamiliar(MiriamWell, player:GetCollectibleNum(CollectibleType.COLLECTIBLE_MIRIAMS_WELL), player:GetCollectibleRNG(CollectibleType.COLLECTIBLE_MIRIAMS_WELL), Isaac.GetItemConfig():GetCollectible(CollectibleType.COLLECTIBLE_MIRIAMS_WELL))
     end
 end
 mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, mod.MiriamCache)
 
-function mod:RespawnWell(player)
-    if RespawnTimer > 0 then
-        RespawnTimer = RespawnTimer - 1
-    end
-    if RespawnTimer == 0 then
-        Isaac.Spawn(EntityType.ENTITY_FAMILIAR, MiriamWell, 0, player.Position, Vector.Zero, player)
-        RespawnTimer = -1
+function mod:WellRender(Well)
+    local data = mod:GetData(Well)
+    local sprite = Well:GetSprite()
+    sprite:Load("gfx/miriams_well.anm2", true)
+    if data.WellDied then
+        if sprite:IsPlaying("Break") == false then
+            sprite:Play("Break", true)
+        end
+    else
+        if sprite:IsPlaying("Idle") == false then
+            sprite:Play("Idle", true)
+        end
     end
 end
-mod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, mod.RespawnWell)
+mod:AddCallback(ModCallbacks.MC_POST_FAMILIAR_RENDER, mod.WellRender, MiriamWell)
