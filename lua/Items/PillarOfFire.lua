119dc6df4a96f1220b84833b09c7fc8195a53a91
goldenstein64 <jamesramsauer@gmail.com>
2022-05-18 16:46:07 -0400

    Localized some variables

--- lua/Items/PillarOfFire.lua
+++ lua/Items/PillarOfFire.lua
@@ -2,32 +2,32 @@ local mod = Furtherance
 local game = Game()
 
 function mod:SpawnFire(entity, damage, flag, source, frames)
-    if entity.Type == EntityType.ENTITY_PLAYER then
-        local player = entity:ToPlayer()
-        if (player and player:HasCollectible(CollectibleType.COLLECTIBLE_PILLAR_OF_FIRE)) then
-            local rng = player:GetCollectibleRNG(CollectibleType.COLLECTIBLE_PILLAR_OF_FIRE)
-            local rollFire = rng:RandomInt(100) + 1
-            if rollFire <= (player.Luck * 5) + 5 then
-                local speed = 4
-                for i = 1, 5 do
-                    if rng:RandomInt(2) == 0 then
-                        RandomVector1 = rng:RandomFloat() * speed
-                    else
-                        RandomVector1 = -rng:RandomFloat() * speed
-                    end
-                    if rng:RandomInt(2) == 0 then
-                        RandomVector2 = rng:RandomFloat() * speed
-                    else
-                        RandomVector2 = -rng:RandomFloat() * speed
-                    end
-                    local fire = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.HOT_BOMB_FIRE, 0, player.Position, Vector(RandomVector1, RandomVector2), player)
-                    fire:ToEffect().Scale = fire:ToEffect().Scale / 1.5
-
+    local player = entity:ToPlayer()
+    if (player and player:HasCollectible(CollectibleType.COLLECTIBLE_PILLAR_OF_FIRE)) then
+        local rng = player:GetCollectibleRNG(CollectibleType.COLLECTIBLE_PILLAR_OF_FIRE)
+        local rollFire = rng:RandomInt(100) + 1
+        if rollFire <= (player.Luck * 5) + 5 then
+            local speed = 4
+            for i = 1, 5 do
+                local RandomVector1, RandomVector2
+                if rng:RandomInt(2) == 0 then
+                    RandomVector1 = rng:RandomFloat() * speed
+                else
+                    RandomVector1 = -rng:RandomFloat() * speed
                 end
+                if rng:RandomInt(2) == 0 then
+                    RandomVector2 = rng:RandomFloat() * speed
+                else
+                    RandomVector2 = -rng:RandomFloat() * speed
+                end
+                local fire = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.HOT_BOMB_FIRE, 0, player.Position, Vector(RandomVector1, RandomVector2), player)
+                fire:ToEffect().Scale = fire:ToEffect().Scale / 1.5
+
             end
         end
     end
 end
+
 mod:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, mod.SpawnFire, EntityType.ENTITY_PLAYER)
 
 local function getNearestEnemy(player)
@@ -64,4 +64,5 @@ function mod:FireTears(entity)
         end
     end
 end
+
 mod:AddCallback(ModCallbacks.MC_POST_EFFECT_UPDATE, mod.FireTears, EffectVariant.HOT_BOMB_FIRE)
