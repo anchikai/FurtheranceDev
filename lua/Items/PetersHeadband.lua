9987cc501168fbfbd5879e5f8a8069ae8180d3b8
anchikai <hentaisuperstar86@gmail.com>
2022-05-20 17:08:39 -0400

    Coded Peter's Headband

--- lua/Items/PetersHeadband.lua
+++ lua/Items/PetersHeadband.lua
@@ -1,3 +1,26 @@
 local mod = Furtherance
 local game = Game()
 
+function mod:Rapturing()
+    local room = game:GetRoom()
+    if room:IsFirstVisit() then
+        RoomCount = RoomCount + 1
+    end
+    for i = 0, game:GetNumPlayers() - 1 do
+        local player = Isaac.GetPlayer(i)
+        if player and player:HasCollectible(CollectibleType.COLLECTIBLE_PETERS_HEADBAND) then
+            if RoomCount >= 12 then
+                RoomCount = 0
+                player:UseActiveItem(CollectibleType.COLLECTIBLE_KEYS_TO_THE_KINGDOM, false, false, true, false, -1)
+            end
+        end
+    end
+end
+mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, mod.Rapturing)
+
+function mod:ResetCounter(continued)
+    if continued == false then
+        RoomCount = 0
+    end
+end
+mod:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, mod.ResetCounter)
