957a793d383d8272d72c8fd94ecec52912f94d24
anchikai <hentaisuperstar86@gmail.com>
2022-05-20 17:20:22 -0400

    Renamed a global

--- lua/Items/PetersHeadband.lua
+++ lua/Items/PetersHeadband.lua
@@ -4,13 +4,13 @@ local game = Game()
 function mod:Rapturing()
     local room = game:GetRoom()
     if room:IsFirstVisit() then
-        RoomCount = RoomCount + 1
+        HeadbandRoomCount = HeadbandRoomCount + 1
     end
     for i = 0, game:GetNumPlayers() - 1 do
         local player = Isaac.GetPlayer(i)
         if player and player:HasCollectible(CollectibleType.COLLECTIBLE_PETERS_HEADBAND) then
-            if RoomCount >= 12 then
-                RoomCount = 0
+            if HeadbandRoomCount >= 12 then
+                HeadbandRoomCount = 0
                 player:UseActiveItem(CollectibleType.COLLECTIBLE_KEYS_TO_THE_KINGDOM, false, false, true, false, -1)
             end
         end
@@ -20,7 +20,7 @@ mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, mod.Rapturing)
 
 function mod:ResetCounter(continued)
     if continued == false then
-        RoomCount = 0
+        HeadbandRoomCount = 0
     end
 end
 mod:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, mod.ResetCounter)
