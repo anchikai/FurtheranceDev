1c245f857f7bbb3a06211d7b873afaa541482c6f
anchikai <hentaisuperstar86@gmail.com>
2022-05-20 17:19:23 -0400

    Coded Peter's Bloody Fracture

--- lua/Items/PetersBloodyFracture.lua
+++ lua/Items/PetersBloodyFracture.lua
@@ -1,3 +1,26 @@
 local mod = Furtherance
 local game = Game()
 
+FractureRoomCount = 6
+function mod:FlippingLogic()
+    local room = game:GetRoom()
+    if room:IsFirstVisit() then
+        FractureRoomCount = FractureRoomCount + 1
+    end
+end
+mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, mod.FlippingLogic)
+
+function mod:ResetCounter(continued)
+    if continued == false then
+        FractureRoomCount = 6
+    end
+end
+mod:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, mod.ResetCounter)
+
+function mod:AcutalFlippage(player)
+    if player:HasCollectible(CollectibleType.COLLECTIBLE_PETERS_BLOODY_FRACTURE) and FractureRoomCount == 6 then
+        FractureRoomCount = 0
+        player:UseActiveItem(CollectibleType.COLLECTIBLE_MUDDLED_CROSS, false, false, true, false, -1)
+    end
+end
+mod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, mod.AcutalFlippage)
