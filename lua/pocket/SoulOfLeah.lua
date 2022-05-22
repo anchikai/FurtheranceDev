31cd8928a94365ba07d35bcfe982b6a247ea1df0
anchikai <hentaisuperstar86@gmail.com>
2022-05-22 02:03:37 -0400

    Renamed data for Leah's Lock

--- lua/pocket/SoulOfLeah.lua
+++ lua/pocket/SoulOfLeah.lua
@@ -23,18 +23,3 @@ function mod:UseSoulOfLeah(card, player, useflags)
 	player:EvaluateItems()
 end
 mod:AddCallback(ModCallbacks.MC_USE_CARD, mod.UseSoulOfLeah, RUNE_SOUL_OF_LEAH)
-
-function mod:solStats(player, flag)
-	local data = mod:GetData(player)
-	if data.solDMG == nil or data.solTR == nil then
-		data.solDMG = 0
-		data.solTR = 0
-	end
-	if flag == CacheFlag.CACHE_FIREDELAY then
-		player.MaxFireDelay = player.MaxFireDelay - (data.solTR * 0.9)
-	end
-	if flag == CacheFlag.CACHE_DAMAGE then
-		player.Damage = player.Damage + data.solDMG
-	end
-end
-mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, mod.solStats)
