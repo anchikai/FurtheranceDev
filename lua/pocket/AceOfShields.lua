09b5d35b71bef2aef0afe751e2897152673d408e
goldenstein64 <jamesramsauer@gmail.com>
2022-05-26 21:41:10 -0400

    Fixed Ace of Shields

--- lua/pocket/AceOfShields.lua
+++ lua/pocket/AceOfShields.lua
@@ -1,15 +1,29 @@
 local mod = Furtherance
 local game = Game()
 
-function mod:UseAceOfShields(card, player, useflags)
-	local data = mod:GetData(player)
+local whitelistedPickupVariants = {
+	[PickupVariant.PICKUP_COLLECTIBLE] = true,
+	[PickupVariant.PICKUP_SHOPITEM] = true,
+	[PickupVariant.PICKUP_BIGCHEST] = true,
+	[PickupVariant.PICKUP_TROPHY] = true,
+	[PickupVariant.PICKUP_BED] = true,
+	[PickupVariant.PICKUP_MOMSCHEST] = true,
+	[PickupVariant.PICKUP_THROWABLEBOMB] = true,
+}
+
+---@param player EntityPlayer
+function mod:UseAceOfShields(_, player)
 	local room = game:GetRoom()
-	for i, entity in ipairs(Isaac.GetRoomEntities()) do
-		if (entity.Type == EntityType.ENTITY_PICKUP and entity.Variant ~= 100 and entity.Variant ~= 150 and entity.Variant ~= 340 and entity.Variant ~= 370 and entity.Variant ~= 380 and entity.Variant ~= 390 and entity.Variant ~= 41) or (entity:IsActiveEnemy(false) and entity:IsDead() == false and entity:IsVulnerableEnemy()) then
-			entity:ToPickup():Morph(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_LIL_BATTERY, BatterySubType.BATTERY_MICRO)
+	for _, entity in ipairs(Isaac.GetRoomEntities()) do
+		local pickup = entity:ToPickup()
+		if pickup and not whitelistedPickupVariants[pickup.Variant] then
+			pickup:Morph(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_LIL_BATTERY, BatterySubType.BATTERY_MICRO)
+		elseif entity:IsActiveEnemy(false) and entity:IsVulnerableEnemy() and not entity:IsBoss() then
+			Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_LIL_BATTERY, BatterySubType.BATTERY_MICRO, entity.Position, Vector.Zero, nil)
+			entity:Remove()
 		end
-		entity.Velocity = Vector.Zero
 		room:SetClear(true)
 	end
 end
+
 mod:AddCallback(ModCallbacks.MC_USE_CARD, mod.UseAceOfShields, CARD_ACE_OF_SHIELDS)
