5bf3b6d207adbfe26882f71f92d7127437e001cf
anchikai <hentaisuperstar86@gmail.com>
2022-05-29 14:03:17 -0400

    Fixed Golden Card being stinky

--- lua/pocket/GoldenCard.lua
+++ lua/pocket/GoldenCard.lua
@@ -3,7 +3,7 @@ local game = Game()
 local rng = RNG()
 
 local function isCard(num)
-    return num < 32 or (num > 41 and num ~= 55)
+    return num < 32 or (num > 41 and num ~= 55 and num ~= 78 and num ~= 80)
 end
 
 local allCards = {}
@@ -19,15 +19,14 @@ end
 
 local processedCards = {}
 
-function mod:SpawnGoldenCard(entityType, variant, _, _, _, spawner, seed)
-    if entityType == EntityType.ENTITY_PICKUP and variant == PickupVariant.PICKUP_TAROTCARD and (spawner == nil or spawner.Type ~= EntityType.ENTITY_PLAYER) and processedCards[seed] == nil then
+function mod:SpawnGoldenCard(entityType, variant, subtype, _, _, spawner, seed)
+    if isCard(subtype) and entityType == EntityType.ENTITY_PICKUP and variant == PickupVariant.PICKUP_TAROTCARD and (spawner == nil or spawner.Type ~= EntityType.ENTITY_PLAYER) and processedCards[seed] == nil then
         processedCards[seed] = true
-        if rng:RandomFloat() <= 0.1 then
+        if rng:RandomFloat() <= 0.025 then
             return { entityType, variant, CARD_GOLDEN, seed }
         end
     end
 end
-
 mod:AddCallback(ModCallbacks.MC_PRE_ENTITY_SPAWN, mod.SpawnGoldenCard)
 
 function mod:ResetProcessedCards()
@@ -35,7 +34,6 @@ function mod:ResetProcessedCards()
         processedCards[seed] = nil
     end
 end
-
 mod:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, mod.ResetProcessedCards)
 
 function mod:UseGoldCard(card, player, flags)
@@ -46,5 +44,4 @@ function mod:UseGoldCard(card, player, flags)
     end
     player:UseCard(pickCard(), UseFlag.USE_NOANIM)
 end
-
 mod:AddCallback(ModCallbacks.MC_USE_CARD, mod.UseGoldCard, CARD_GOLDEN)
