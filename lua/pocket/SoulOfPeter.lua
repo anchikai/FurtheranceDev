67810b40194a8a8a486ae61755776bea1dd9bb26
goldenstein64 <jamesramsauer@gmail.com>
2022-06-07 03:36:35 -0400

    Soul of Peter "runs better"

--- lua/pocket/SoulOfPeter.lua
+++ lua/pocket/SoulOfPeter.lua
@@ -2,7 +2,7 @@ local mod = Furtherance
 local game = Game()
 local rng = RNG()
 
-local function RoomGenerator(index, slot, newroom)
+local function RoomGenerator(index, slot, newRoomIndex)
     local level = game:GetLevel()
     local OldStage, OldStageType, OldChallenge = level:GetStage(), level:GetStageType(), game.Challenge
     -- Set to Basement 1
@@ -12,7 +12,7 @@ local function RoomGenerator(index, slot, newroom)
     -- Make the room
     level:MakeRedRoomDoor(index, slot)
 
-    RedRoom = level:GetRoomByIdx(newroom, 0)
+    RedRoom = level:GetRoomByIdx(newRoomIndex, 0)
     RedRoom.Flags = 0
     RedRoom.DisplayFlags = 0
 
@@ -22,48 +22,77 @@ local function RoomGenerator(index, slot, newroom)
     level:UpdateVisibility()
 end
 
-local function pickRandomRoom(roomsList, level, room)
-    local RandomRooms = {}
+local roomNeighborOffsets = { 1, 13, -1, -13 }
+local doorMap = {
+    [-1] = 0,
+    [-13] = 1,
+    [1] = 2,
+    [13] = 3
+}
+
+local function getPossibleRoomNeighbors(roomsList, level)
+    local roomNeighbors = {}
+    local visitedRooms = {}
     for i = 0, #roomsList - 1 do
         local roomDesc = roomsList:Get(i)
-        if roomDesc.Data.Type ~= RoomType.ROOM_ULTRASECRET and roomDesc.Data.GridIndex ~= level:GetCurrentRoomIndex() then
-            table.insert(RandomRooms, roomDesc)
+        if roomDesc.Data.Type ~= RoomType.ROOM_SECRET and roomDesc.Data.Type ~= RoomType.ROOM_SUPERSECRET and roomDesc.Data.Type ~= RoomType.ROOM_ULTRASECRET then
+            for _, idxOffset in ipairs(roomNeighborOffsets) do
+                local idx = roomDesc.GridIndex + idxOffset
+                local roomNeighbor = level:GetRoomByIdx(idx)
+                if idx >= 0 and idx <= 168 and roomNeighbor.GridIndex == -1 and not visitedRooms[idx] then -- room doesn't exist
+                    visitedRooms[idx] = true
+                    table.insert(roomNeighbors, {roomDesc.GridIndex, idxOffset})
+                end
+            end
         end
     end
+
+    return roomNeighbors
+end
+
+local function pickRoomNeighbor(RandomRooms)
     if #RandomRooms > 0 then
         local choice = rng:RandomInt(#RandomRooms) + 1
-        return RandomRooms[choice].GridIndex
+        return table.remove(RandomRooms, choice)
+    else
+        return nil
     end
 end
 
 function mod:UseSoulOfPeter(card, player, flag)
     local level = game:GetLevel()
-    local room = game:GetRoom()
+    -- local room = game:GetRoom()
     local roomsList = level:GetRooms()
-    local door = rng:RandomInt(4)
-    local doorIndex
-    if door == 0 then
-        doorIndex = -1
-    elseif door == 1 then
-        doorIndex = -13
-    elseif door == 2 then
-        doorIndex = 1
-    elseif door == 3 then
-        doorIndex = 13
-    end
-    local RoomCount = 0
-    for _ = 0, 168 do
-        if RoomCount >= 5 then break end
-        local RandRoomIdx = pickRandomRoom(roomsList, level, room)
-        if RandRoomIdx ~= level:GetCurrentRoomIndex() then
-            RoomGenerator(RandRoomIdx, door, RandRoomIdx+doorIndex)
-            print("Made room! "..RandRoomIdx)
-            local NewRoomIdx = level:GetRoomByIdx(RandRoomIdx+doorIndex)
-            NewRoomIdx.DisplayFlags = 101
+    -- local door = rng:RandomInt(4)
+    -- local doorIndex
+    -- if door == 0 then
+    --     doorIndex = -1
+    -- elseif door == 1 then
+    --     doorIndex = -13
+    -- elseif door == 2 then
+    --     doorIndex = 1
+    -- elseif door == 3 then
+    --     doorIndex = 13
+    -- end
+    local randomRooms = getPossibleRoomNeighbors(roomsList, level)
+    for _ = 1, 5 do
+        local randRoomInfo = pickRoomNeighbor(randomRooms)
+        if randRoomInfo then
+            local roomIdx, idxOffset = randRoomInfo[1], randRoomInfo[2]
+            RoomGenerator(roomIdx, doorMap[idxOffset], roomIdx + idxOffset)
+            print("Made room! "..(roomIdx + idxOffset))
+            local NewRoom = level:GetRoomByIdx(roomIdx + idxOffset)
+            NewRoom.DisplayFlags = 101
             level:UpdateVisibility()
         end
-        RoomCount = RoomCount + 1
-        print(RoomCount)
+
+        -- if randRoom ~= level:GetCurrentRoomDesc().GridIndex then
+        --     RoomGenerator(randRoom, door, randRoom+doorIndex)
+        --     print("Made room! "..randRoom)
+        --     local NewRoomIdx = level:GetRoomByIdx(randRoom+doorIndex)
+        --     NewRoomIdx.DisplayFlags = 101
+        --     level:UpdateVisibility()
+        -- end
     end
 end
 mod:AddCallback(ModCallbacks.MC_USE_CARD, mod.UseSoulOfPeter, RUNE_SOUL_OF_PETER)
