811c58a75160b96d8a130aef20645941bf36e54b
goldenstein64 <jamesramsauer@gmail.com>
2022-06-07 19:20:36 -0400

    WIP Leah's Lock rework

--- lua/items/trinkets/LeahsLock.lua
+++ lua/items/trinkets/LeahsLock.lua
@@ -2,40 +2,20 @@ local mod = Furtherance
 local game = Game()
 local BrokenHeartbeatSound = Isaac.GetSoundIdByName("BrokenHeartbeat")
 
-mod:SavePlayerData({
-	LeahsLockTears = 0
-})
+---@param tear EntityTear
+function mod:FireLLTears(tear)
+	local player = tear.SpawnerEntity and tear.SpawnerEntity:ToPlayer()
+	if player == nil or not player:HasTrinket(TrinketType.TRINKET_LEAHS_LOCK) then return end
 
-function mod:LeahsLock()
-	local room = game:GetRoom()
-	for i = 0, game:GetNumPlayers() - 1 do
-		local player = Isaac.GetPlayer(i)
-		local data = mod:GetData(player)
-		local rng = player:GetTrinketRNG(TrinketType.TRINKET_LEAHS_LOCK)
-		if player:HasTrinket(TrinketType.TRINKET_LEAHS_LOCK) and room:IsFirstVisit() then
-			if rng:RandomFloat() < 0.5 and player:GetBrokenHearts() > 0 then
-				SFXManager():Play(SoundEffect.SOUND_HEARTBEAT)
-				player:AddBrokenHearts(-1)
-				data.LeahsLockTears = data.LeahsLockTears - 1
-			elseif player.MaxFireDelay > 2 then
-				SFXManager():Play(BrokenHeartbeatSound)
-				player:AddBrokenHearts(1)
-				data.LeahsLockTears = data.LeahsLockTears + 1
-			end
-		end
-		player:AddCacheFlags(CacheFlag.CACHE_FIREDELAY)
-		player:EvaluateItems()
+	local rng = player:GetTrinketRNG(TrinketType.TRINKET_LEAHS_LOCK)
+	local chance = 0.25 + math.min(player.Luck * 0.025, 0.25)
+	local choice = rng:RandomFloat()
+	if choice < chance / 2 then
+		tear:AddTearFlags(TearFlags.TEAR_CHARM)
+		tear:SetColor(Color(1, 0, 1, 1, 0.196, 0, 0), 0, 1)
+	elseif choice < chance then
+		tear:AddTearFlags(TearFlags.TEAR_FEAR)
+		tear:SetColor(Color(1, 1, 0.455, 1, 0.169, 0.145, 0), 0, 1)
 	end
 end
-mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, mod.LeahsLock)
-
-function mod:llStats(player, flag)
-	local data = mod:GetData(player)
-	if data.LeahsLockTears == nil then return end
-	if player:HasTrinket(TrinketType.TRINKET_LEAHS_LOCK) then
-		if flag == CacheFlag.CACHE_FIREDELAY then
-			player.MaxFireDelay = player.MaxFireDelay - (data.LeahsLockTears * 0.9)
-		end
-	end
-end
-mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, mod.llStats)
+mod:AddCallback(ModCallbacks.MC_POST_FIRE_TEAR, mod.FireLLTears)
