b04f26ec5e6847fefa5a2924a8312136e639e0f4
anchikai <hentaisuperstar86@gmail.com>
2022-06-07 18:59:28 -0400

    Blood Cyst is a familiar now + BFFS! Synergy

--- lua/items/collectibles/BloodCyst.lua
+++ lua/items/collectibles/BloodCyst.lua
@@ -1,6 +1,5 @@
 local mod = Furtherance
 local game = Game()
-local level = game:GetLevel()
 
 local Cyst = Isaac.GetEntityVariantByName("Blood Cyst")
 
@@ -9,18 +8,34 @@ function mod:RespawnCyst()
     for i = 0, game:GetNumPlayers() - 1 do
         local player = Isaac.GetPlayer(i)
         if room:IsClear() == false and player:HasCollectible(CollectibleType.COLLECTIBLE_BLOOD_CYST) then
-            Isaac.Spawn(EntityType.ENTITY_BOIL, Cyst, 0, Isaac.GetFreeNearPosition(room:GetRandomPosition(0), 40), Vector.Zero, player)
+            Isaac.Spawn(EntityType.ENTITY_FAMILIAR, Cyst, 0, Isaac.GetFreeNearPosition(room:GetRandomPosition(0), 40), Vector.Zero, player)
+        elseif room:IsClear() then
+            for _, entity in ipairs(Isaac.GetRoomEntities()) do
+                if entity.Type == EntityType.ENTITY_FAMILIAR and entity.Variant == Cyst then
+                    entity:Remove()
+                end
+            end
         end
     end
 end
 mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, mod.RespawnCyst)
 
-function mod:BloodCystBurst(bloodcyst, collider)
-    if bloodcyst.Variant == Cyst and (collider.SpawnerType == EntityType.ENTITY_PLAYER or collider.Type == EntityType.ENTITY_PLAYER) then
-        bloodcyst:Die()
-        for i = 1, 8 do
-            Isaac.Spawn(EntityType.ENTITY_TEAR, TearVariant.BLOOD, 0, bloodcyst.Position, Vector(8, 0):Rotated(i * 45), bloodcyst)
+function mod:BloodCystBurst(bloodcyst)
+    local player = bloodcyst.SpawnerEntity:ToPlayer()
+    for _, entity in ipairs(Isaac.FindInRadius(bloodcyst.Position, 15, EntityPartition.TEAR)) do
+        if entity.Type == EntityType.ENTITY_TEAR then
+            bloodcyst:Die()
+            entity:Die()
+            for i = 1, 8 do
+                if player:HasCollectible(CollectibleType.COLLECTIBLE_BLOOD_CYST) and player:HasCollectible(CollectibleType.COLLECTIBLE_BFFS) then
+                    local CystTears = Isaac.Spawn(EntityType.ENTITY_TEAR, TearVariant.BLOOD, 0, bloodcyst.Position, Vector(8, 0):Rotated(i * 45), bloodcyst):ToTear()
+                    CystTears.CollisionDamage = 7
+                    CystTears.Scale = 1.1
+                else
+                    Isaac.Spawn(EntityType.ENTITY_TEAR, TearVariant.BLOOD, 0, bloodcyst.Position, Vector(8, 0):Rotated(i * 45), bloodcyst)
+                end
+            end
         end
     end
 end
-mod:AddCallback(ModCallbacks.MC_PRE_NPC_COLLISION, mod.BloodCystBurst, EntityType.ENTITY_BOIL)
+mod:AddCallback(ModCallbacks.MC_FAMILIAR_UPDATE, mod.BloodCystBurst, Cyst)
