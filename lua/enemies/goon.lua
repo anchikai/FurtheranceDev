9872b71a220993b80e5b2ebbcb77975a80b1ef3a
PixelPlz <101890162+PixelPlz@users.noreply.github.com>
2022-06-11 18:50:47 +0200

    goons

--- lua/enemies/goon.lua
+++ lua/enemies/goon.lua
@@ -3,17 +3,20 @@ local game = Game()
 local rng = RNG()
 
 local Settings = {
-	MoveSpeed = 4,
-	Range = 100,
+	MoveSpeed = 4.25,
+	Range = 120,
 	SlowAmount  = 0.1,
-	ShakeThreshold = 30,
-	NeededShakes = 10
+	ShakeThreshold = 10,
+	NeededShakes = 10,
+	StunTime = 20,
+	RandomOffset = 10
 }
 
 local States = {
-	Chilling = 0,
-	Chasing = 1,
-	Attached = 2
+	Appear = 0,
+	Chilling = 1,
+	Chasing = 2,
+	Attached = 3
 }
 
 
@@ -51,8 +54,17 @@ function mod:GoonInit(entity)
 			sprite:ReplaceSpritesheet(2, "gfx/monsters/goons/" .. goonType .. "goon_nose_" .. noseColor .. ".png")
 		end
 		sprite:LoadGraphics()
-
-		data.state = States.Chilling
+		
+		-- Multiple spawner
+		if entity.SubType > 0 then
+			for i = 0, entity.SubType - 1 do -- High amounts near rocks will most likely result in some of them spawning on top of the rocks!
+				local getPos = Vector(math.random(-Settings.RandomOffset, Settings.RandomOffset), math.random(-Settings.RandomOffset, Settings.RandomOffset))
+				Isaac.Spawn(entity.Type, entity.Variant, 0, entity.Position + getPos, Vector.Zero, nil)
+			end
+			entity.Position = entity.Position + Vector(math.random(-Settings.RandomOffset, Settings.RandomOffset), math.random(-Settings.RandomOffset, Settings.RandomOffset))
+			entity.SubType = 0
+		end
+		data.state = States.Appear
 	end
 end
 mod:AddCallback(ModCallbacks.MC_POST_NPC_INIT, mod.GoonInit, 200)
@@ -64,8 +76,10 @@ function mod:GoonUpdate(entity)
 		local sprite = entity:GetSprite()
 
 
-		if data.state == States.Chilling then
+		if data.state == States.Appear then
 			entity.Velocity = Vector.Zero
+			data.state = States.Chilling
+
 			if not sprite:IsPlaying("Idle") and not sprite:IsPlaying("GetUp") then
 				sprite:Play("Idle", true)
 			end
@@ -73,14 +87,32 @@ function mod:GoonUpdate(entity)
 				sprite.FlipX = true
 			end
 
+
+		elseif data.state == States.Chilling then
+			entity.EntityCollisionClass = EntityCollisionClass.ENTCOLL_ALL
+			sprite.Offset = Vector.Zero
+			entity.Velocity = (entity.Velocity + (Vector.Zero - entity.Velocity) * 0.25)
+
+			if not sprite:IsPlaying("Idle") and not sprite:IsPlaying("GetUp") then
+				sprite:Play("Idle", true)
+			end
+			
+			if data.timer then
+				if data.timer <= 0 then
+					data.timer = nil
+				else
+					data.timer = data.timer - 1
+				end
+			end
+
 			-- Get up
-			if entity.Position:Distance(target.Position) <= Settings.Range then
+			if entity.Position:Distance(target.Position) <= Settings.Range and not data.timer then
 				if not sprite:IsPlaying("GetUp") then
 					sprite:Play("GetUp", true)
 				end
-				if sprite:GetFrame() == 6 then
-					data.state = States.Chasing
-				end
+			end
+			if sprite:IsPlaying("GetUp") and sprite:GetFrame() == 6 then
+				data.state = States.Chasing
 			end
 
 
@@ -135,6 +167,7 @@ function mod:GoonUpdate(entity)
 			entity.Position = entity.Parent.Position
 			entity.Velocity = entity.Parent.Velocity
 
+			sprite:RemoveOverlay()
 			entity.DepthOffset = entity.Parent.DepthOffset + 10
 			if not sprite:IsPlaying("Cling") then
 				sprite:Play("Cling", true)
@@ -148,39 +181,54 @@ function mod:GoonUpdate(entity)
 				entity.Parent:AddSlowing(EntityRef(entity), 1, 0.1, Color(1,1,1,1,0,0,0))
 			end
 
-			-- Get random first needed direction
-			if not data.needDir then
-				data.needDir = 0 + (math.random(0,1) * 2)
-			end
-
-			-- Check if player is moving back and forth
-			if entity.Parent:ToPlayer():GetMovementDirection() == data.needDir then
-				if data.shakes then
-					data.shakes = data.shakes + 1
-				else
-					data.shakes = 1
+			if entity.Index == entity.Parent:GetData().goonies[1] then
+				-- Get random first needed direction
+				if not data.needDir then
+					data.needDir = 0 + (math.random(0,1) * 2)
 				end
 
-				if data.needDir == 0 then
-					data.needDir = 2
-				else
-					data.needDir = 0
+				-- Check if player is moving back and forth
+				if entity.Parent:ToPlayer():GetMovementDirection() == data.needDir then
+					if data.shakes then
+						data.shakes = data.shakes + 1
+					else
+						data.shakes = 1
+					end
+
+					if data.needDir == 0 then
+						data.needDir = 2
+					else
+						data.needDir = 0
+					end
+					
+					data.timer = Settings.ShakeThreshold
+				end
+
+				-- Reset shake count if player is too slow
+				if data.timer then 
+					if data.timer <= 0 then
+						data.shakes = 0
+					else
+						data.timer = data.timer - 1
+					end
 				end
 				
-				data.timer = Settings.ShakeThreshold
-			end
-
-			-- Reset shake count if player is too slow
-			if data.timer then 
-				if data.timer <= 0 then
+				-- Fall off
+				if data.shakes and data.shakes >= Settings.NeededShakes then
 					data.shakes = 0
-				else
-					data.timer = data.timer - 1
+					table.remove(entity.Parent:GetData().goonies, 1)
+					data.state = States.Chilling
+					entity.Parent:ClearEntityFlags(EntityFlag.FLAG_SLOW)
+
+					local mult = 1
+					if sprite.FlipX == false then
+						mult = -1
+					end
+					entity.Velocity = Vector(mult * 15, math.random(-10,10))
+
+					data.timer = Settings.StunTime
+					SFXManager():Play(SoundEffect.SOUND_BABY_HURT)
 				end
-			end
-			
-			if data.shakes and data.shakes >= Settings.NeededShakes then
-				entity:TakeDamage(entity.MaxHitPoints + 1, 0, EntityRef(entity.Parent), 0)
 			end
 		end
 
@@ -200,7 +248,7 @@ mod:AddCallback(ModCallbacks.MC_NPC_UPDATE, mod.GoonUpdate, 200)
 
 -- Attach to player
 function mod:GoonCollide(goon, collider)
-	if goon.Variant == 3071 and collider.Type == EntityType.ENTITY_PLAYER then
+	if goon.Variant == 3071 and collider.Type == EntityType.ENTITY_PLAYER and goon:GetData().state == States.Chasing then
 		if not collider:GetData().goonies then
 			collider:GetData().goonies = {}
 		end
@@ -217,9 +265,8 @@ function mod:GoonCollide(goon, collider)
 				table.insert(collider:GetData().goonies, goon.Index)
 				goon.Parent = collider
 				goon.EntityCollisionClass = EntityCollisionClass.ENTCOLL_NONE
-				goon:GetSprite():RemoveOverlay()
 				goon:GetData().state = States.Attached
-				SFXManager():Play(SoundEffect.SOUND_BABY_HURT, 0.9)
+				SFXManager():Play(SoundEffect.SOUND_KISS_LIPS1, 1.2)
 			end
 		end
 	end
