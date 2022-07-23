5476e9806f9948c18035aefa114ab6b60b50aded
anchikai <hentaisuperstar86@gmail.com>
2022-07-23 09:58:30 -0400

    Polydipsia interaction fixes

--- lua/items/collectibles/Polydipsia.lua
+++ lua/items/collectibles/Polydipsia.lua
@@ -1,6 +1,5 @@
 local mod = Furtherance
 local game = Game()
-local rng = RNG()
 
 local WhirlpoolVariant = Isaac.GetEntityVariantByName("Miriam Whirlpool")
 
@@ -20,7 +19,7 @@ function mod:GetPolydipsia(player, cacheFlag)
 		end
 	end
 
-	if hasItem(player) and not isMiriam(player) then
+	if hasItem(player) and not isMiriam(player) and player:GetPlayerType() ~= PlayerType.PLAYER_THEFORGOTTEN then
 		if cacheFlag == CacheFlag.CACHE_FIREDELAY then
 			player.MaxFireDelay = (player.MaxFireDelay * 2) + 10
 		end
@@ -38,9 +37,24 @@ local function makeMiriamPuddle(miriam, tear)
 	else
 		local puddle = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.PLAYER_CREEP_HOLYWATER_TRAIL, 1, tear.Position, Vector.Zero, miriam):ToEffect()
 		puddle.CollisionDamage = miriam.Damage * 0.33
-
 		puddle.SpriteScale = Vector.One * playerData.MiriamAOE
 		puddle.Scale = playerData.MiriamAOE
+		if miriam:HasCollectible(CollectibleType.COLLECTIBLE_CHOCOLATE_MILK) then
+			puddle.Color = Color(0.75, 0.25, 0.05, 1)
+		end
+		if miriam:HasCollectible(CollectibleType.COLLECTIBLE_TECHNOLOGY) or miriam:HasCollectible(CollectibleType.COLLECTIBLE_BRIMSTONE) then
+			puddle.Color = Color(1, 0, 0, 1)
+			if miriam:HasCollectible(CollectibleType.COLLECTIBLE_BRIMSTONE) then
+				puddle.CollisionDamage = miriam.Damage * 0.4
+			end
+		end
+		if miriam:HasCollectible(CollectibleType.COLLECTIBLE_SOY_MILK) then
+			local color = Color(1, 1, 1, 1, 0, 0, 0)
+    		color:SetColorize(2, 2, 2, 1)
+			puddle.Color = color
+			puddle.SpriteScale = Vector.One * playerData.MiriamAOE/2
+			puddle.Scale = playerData.MiriamAOE/2
+		end
 	end
 
 	if hasItem(miriam) and tear.SubType == 0 then
@@ -71,3 +85,36 @@ function mod:MakePolydipsiaTear(tear)
     end
 end
 mod:AddCallback(ModCallbacks.MC_POST_FIRE_TEAR, mod.MakePolydipsiaTear)
+
+-- Dr. Fetus Synergy
+function mod:PolyBombUpdate(bomb)
+	local player = mod:GetPlayerFromTear(bomb)
+	local data = mod:GetData(bomb)
+	if player then
+		if bomb.FrameCount == 1 then
+			if bomb.Type == EntityType.ENTITY_BOMB and bomb.Variant ~= BombVariant.BOMB_THROWABLE
+			and (player:HasCollectible(CollectibleType.COLLECTIBLE_POLYDIPSIA) or isMiriam(player)) then
+				if data.isPolyBomb == nil then
+					data.isPolyBomb = true
+				end
+			end
+		end
+	end
+	if data.isPolyBomb then
+		local sprite = bomb:GetSprite()
+		if sprite:IsPlaying("Explode") then
+			makeMiriamPuddle(player, bomb)
+		end
+	end
+end
+mod:AddCallback(ModCallbacks.MC_POST_BOMB_UPDATE, mod.PolyBombUpdate)
+
+-- Brimstone & Tech "Synergy"
+function mod:PolyLasers(laser)
+	local player = laser.SpawnerEntity and laser.SpawnerEntity:ToPlayer()
+	if (player:HasCollectible(CollectibleType.COLLECTIBLE_POLYDIPSIA) or isMiriam(player)) and laser.FrameCount == 1 then
+		makeMiriamPuddle(player, laser)
+	end
+end
+
+mod:AddCallback(ModCallbacks.MC_POST_LASER_UPDATE, mod.PolyLasers)
