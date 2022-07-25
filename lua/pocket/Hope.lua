88192bdc7957fb0be8f2bfd977c07d4b29f68e37
anchikai <hentaisuperstar86@gmail.com>
2022-07-25 02:29:19 -0400

    Finished the cards

--- lua/pocket/Hope.lua
+++ lua/pocket/Hope.lua
@@ -1,6 +1,22 @@
 local mod = Furtherance
+local rng = RNG()
 
+local HopeActive = false
 function mod:UseHope(card, player, flag)
-	
+	HopeActive = true
 end
 mod:AddCallback(ModCallbacks.MC_USE_CARD, mod.UseHope, CARD_HOPE)
+
+function mod:NewRoom()
+	HopeActive = false
+end
+mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, mod.NewRoom)
+
+function mod:HopeKills(entity)
+	if HopeActive then
+        if rng:RandomFloat() <= 0.2 then
+            Isaac.Spawn(EntityType.ENTITY_PICKUP, 0, 0, entity.Position, Vector.Zero, player)
+        end
+    end
+end
+mod:AddCallback(ModCallbacks.MC_POST_ENTITY_KILL, mod.HopeKills)
--- lua/pocket/ReverseFaith.lua
+++ lua/pocket/ReverseFaith.lua
@@ -1,6 +1,8 @@
 local mod = Furtherance
 
 function mod:UseReverseFaith(card, player, flag)
-	
+	for _ = 1, 2 do
+        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_HEART, HeartSubType.HEART_MOON, Isaac.GetFreeNearPosition(player.Position, 40), Vector.Zero, player)
+    end
 end
 mod:AddCallback(ModCallbacks.MC_USE_CARD, mod.UseReverseFaith, CARD_REVERSE_FAITH)
