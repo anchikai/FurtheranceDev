90d35623cf854d6b977a15711609189521787a01
anchikai <hentaisuperstar86@gmail.com>
2022-07-25 03:26:04 -0400

    Finished Rotten Love

---
+++ lua/items/collectibles/RottenLove.lua
@@ -0,0 +1,27 @@
+local mod = Furtherance
+
+function mod:SetRottenLoveData(player)
+    local data = mod:GetData(player)
+    data.OldRottenLoveCount = player:GetCollectibleNum(CollectibleType.COLLECTIBLE_ROTTEN_LOVE)
+end
+mod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, mod.SetRottenLoveData)
+
+function mod:GiveHeartsOnPickUp(player)
+    local data = mod:GetData(player)
+    local newRottenLoveCount = player:GetCollectibleNum(CollectibleType.COLLECTIBLE_ROTTEN_LOVE)
+    if data.OldRottenLoveCount == newRottenLoveCount then return end
+
+    local delta = newRottenLoveCount - data.OldRottenLoveCount
+    data.OldRottenLoveCount = newRottenLoveCount
+
+    local rng = player:GetCollectibleRNG(CollectibleType.COLLECTIBLE_ROTTEN_LOVE)
+    for _ = 1, delta do
+        for _ = 1, rng:RandomInt(2)+2 do
+            Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_HEART, HeartSubType.HEART_BONE, Isaac.GetFreeNearPosition(player.Position, 40), Vector.Zero, player)
+        end
+        for _ = 1, rng:RandomInt(2)+2 do
+            Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_HEART, HeartSubType.HEART_ROTTEN, Isaac.GetFreeNearPosition(player.Position, 40), Vector.Zero, player)
+        end
+    end
+end
+mod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, mod.GiveHeartsOnPickUp)
