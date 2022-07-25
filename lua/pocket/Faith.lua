d75e19391df293aa308c19c2dabba13b83f81f67
anchikai <hentaisuperstar86@gmail.com>
2022-07-25 13:43:45 -0400

    Cleaned up the rest of the cards

--- lua/pocket/Faith.lua
+++ lua/pocket/Faith.lua
@@ -1,6 +1,9 @@
 local mod = Furtherance
 
 function mod:UseFaith(card, player, flag)
-	Isaac.Spawn(EntityType.ENTITY_SLOT, 17, 0, Isaac.GetFreeNearPosition(player.Position, 40), Vector.Zero, player)
+	local confess = Isaac.Spawn(EntityType.ENTITY_SLOT, 17, 0, Isaac.GetFreeNearPosition(player.Position, 40), Vector.Zero, player)
+	local smoke = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, 3, confess.Position, Vector.Zero, nil):GetSprite()
+	SFXManager():Stop(SoundEffect.SOUND_FART)
+	SFXManager():Play(SoundEffect.SOUND_SUMMONSOUND)
 end
 mod:AddCallback(ModCallbacks.MC_USE_CARD, mod.UseFaith, CARD_FAITH)
--- lua/pocket/ReverseHope.lua
+++ lua/pocket/ReverseHope.lua
@@ -1,7 +1,11 @@
 local mod = Furtherance
+local game = Game()
 local rng = RNG()
 
 function mod:UseReverseHope(card, player, flag)
+	local level = game:GetLevel()
 	Isaac.ExecuteCommand("goto s.challenge."..rng:RandomInt(25))
+	level.LeaveDoor = -1
+    game:StartRoomTransition(-3, Direction.NO_DIRECTION, RoomTransitionAnim.TELEPORT, player, -1)
 end
 mod:AddCallback(ModCallbacks.MC_USE_CARD, mod.UseReverseHope, CARD_REVERSE_HOPE)
