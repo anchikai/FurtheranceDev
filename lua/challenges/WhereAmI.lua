ca38d3b59c8ca586720dcecb96e8aed2d09e81c4
anchikai <hentaisuperstar86@gmail.com>
2022-07-25 16:03:50 -0400

    rewrote the fucking terrible "Where Am I?" code

--- lua/challenges/WhereAmI.lua
+++ lua/challenges/WhereAmI.lua
@@ -2,20 +2,14 @@ local mod = Furtherance
 local game = Game()
 local WhereAmI = Isaac.GetChallengeIdByName("Where am I?")
 
-local function clamp(value, min, max)
-	return math.min(math.max(value, min), max)
-end
-
 function mod:RandomStage(player)
     local level = game:GetLevel()
-    local room = game:GetRoom()
     local rng = player:GetCollectibleRNG(CollectibleType.COLLECTIBLE_ALTERNATE_REALITY)
+    local sprite = player:GetSprite()
     if Isaac.GetChallenge() == WhereAmI then
-       if room:GetType() == RoomType.ROOM_BOSS and room:IsClear() and level:GetStage() < LevelStage.STAGE4_2 then
-            if player.Position.X == clamp(player.Position.X, 297, 343) and player.Position.Y == clamp(player.Position.Y, 177, 223) and player:IsHoldingItem() == false then
-                level:SetStage(rng:RandomInt(7)+1, rng:RandomInt(3))
-            end
-       end
+        if sprite:IsPlaying("Trapdoor") and sprite:GetFrame() == 15 then
+            level:SetStage(rng:RandomInt(7)+1, rng:RandomInt(3))
+        end
     end
 end
-mod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, mod.RandomStage)
+mod:AddCallback(ModCallbacks.MC_POST_PLAYER_RENDER, mod.RandomStage)
