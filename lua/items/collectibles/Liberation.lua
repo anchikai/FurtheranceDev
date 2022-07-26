03c1c3c162ec7aabb341d8ba6df90a0131cb0847
goldenstein64 <goldnstein64@gmail.com>
2022-07-25 20:28:58 -0400

    Fixed liberation banning flight

--- lua/items/collectibles/Liberation.lua
+++ lua/items/collectibles/Liberation.lua
@@ -13,12 +13,11 @@ function mod:LibKill(entity)
         local data = mod:GetData(player)
         local rng = player:GetCollectibleRNG(CollectibleType.COLLECTIBLE_LIBERATION)
         if player:HasCollectible(CollectibleType.COLLECTIBLE_LIBERATION) and rng:RandomFloat() <= 10.05 then
-            if data.LiberationCanFly == false then
+            if data.ActivatedLiberation == false then
+                player:UseActiveItem(CollectibleType.COLLECTIBLE_BIBLE, false, false, true, true, -1)
                 player:UseActiveItem(CollectibleType.COLLECTIBLE_DADS_KEY, false, false, true, false, -1)
             end
-            data.LiberationCanFly = true
-            player:AddCacheFlags(CacheFlag.CACHE_FLYING)
-            player:EvaluateItems()
+            data.ActivatedLiberation = true
         end
     end
 end
@@ -28,19 +27,7 @@ function mod:LibNewRoom()
     for p = 0, game:GetNumPlayers() - 1 do
 		local player = Isaac.GetPlayer(p)
         local data = mod:GetData(player)
-        data.LiberationCanFly = false
-        player:AddCacheFlags(CacheFlag.CACHE_FLYING)
-        player:EvaluateItems()
+        data.ActivatedLiberation = false
     end
 end
 mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, mod.LibNewRoom)
-
-function mod:LibCache(player, flag)
-    local data = mod:GetData(player)
-	if data.LiberationCanFly then
-		player.CanFly = true
-    else
-        player.CanFly = false
-    end
-end
-mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, mod.LibCache, CacheFlag.CACHE_FLYING)
