0b7f6cbd5bbcf7c78ee2496038e25bf28eebf7c1
goldenstein64 <goldnstein64@gmail.com>
2022-07-27 00:19:13 -0400

    Fixed Liberation always proccing

--- lua/items/collectibles/Liberation.lua
+++ lua/items/collectibles/Liberation.lua
@@ -1,22 +1,14 @@
 local mod = Furtherance
 local game = Game()
 
-function mod:SetLiberationData(player)
-    local data = mod:GetData(player)
-    data.LiberationCanFly = false
-end
-mod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, mod.SetLiberationData)
-
 function mod:LibKill(entity)
     for p = 0, game:GetNumPlayers() - 1 do
 		local player = Isaac.GetPlayer(p)
         local data = mod:GetData(player)
         local rng = player:GetCollectibleRNG(CollectibleType.COLLECTIBLE_LIBERATION)
-        if player:HasCollectible(CollectibleType.COLLECTIBLE_LIBERATION) and rng:RandomFloat() <= 10.05 then
-            if data.ActivatedLiberation == false then
-                player:UseActiveItem(CollectibleType.COLLECTIBLE_BIBLE, false, false, true, true, -1)
-                player:UseActiveItem(CollectibleType.COLLECTIBLE_DADS_KEY, false, false, true, false, -1)
-            end
+        if player:HasCollectible(CollectibleType.COLLECTIBLE_LIBERATION) and not data.ActivatedLiberation and rng:RandomFloat() <= 0.05 then
+            player:UseActiveItem(CollectibleType.COLLECTIBLE_BIBLE, false, false, true, true, -1)
+            player:UseActiveItem(CollectibleType.COLLECTIBLE_DADS_KEY, false, false, true, false, -1)
             data.ActivatedLiberation = true
         end
     end
