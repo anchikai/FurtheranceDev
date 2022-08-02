01dd731187d92a281525c860af3156fc1f7d0ae0
goldenstein64 <goldnstein64@gmail.com>
2022-08-01 20:23:05 -0400

    golden card uses InitSeed RNG instead of a set

--- lua/pocket/GoldenCard.lua
+++ lua/pocket/GoldenCard.lua
@@ -1,6 +1,4 @@
 local mod = Furtherance
-local game = Game()
-local rng = RNG()
 
 local function isCard(num)
     return (num < 32 or num > 41) and num ~= 55 and num ~= 78 and num ~= 80
@@ -18,17 +16,15 @@ for i = 1, Isaac.GetItemConfig():GetCards().Size do
     end
 end
 
-local function pickCard()
+local function pickCard(rng)
     return allCards[rng:RandomInt(#allCards) + 1]
 end
 
-local processedCards = {}
-
 function mod:SpawnGoldenCard(entityType, variant, subtype, _, _, spawner, seed)
-    local room = game:GetRoom()
-    if isCard(subtype) and entityType == EntityType.ENTITY_PICKUP and variant == PickupVariant.PICKUP_TAROTCARD
-    and (spawner == nil or spawner.Type ~= EntityType.ENTITY_PLAYER) and processedCards[seed] == nil and room:IsFirstVisit() then
-        processedCards[seed] = true
+    if entityType == EntityType.ENTITY_PICKUP and variant == PickupVariant.PICKUP_TAROTCARD and isCard(subtype)
+    and (spawner == nil or spawner.Type ~= EntityType.ENTITY_PLAYER) then
+        local rng = RNG()
+        rng:SetSeed(seed, 1)
         if rng:RandomFloat() <= 0.025 then
             return { entityType, variant, CARD_GOLDEN, seed }
         end
@@ -36,19 +32,13 @@ function mod:SpawnGoldenCard(entityType, variant, subtype, _, _, spawner, seed)
 end
 mod:AddCallback(ModCallbacks.MC_PRE_ENTITY_SPAWN, mod.SpawnGoldenCard)
 
-function mod:ResetProcessedCards()
-    for seed in pairs(processedCards) do
-        processedCards[seed] = nil
-    end
-end
-mod:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, mod.ResetProcessedCards)
-
 function mod:UseGoldCard(card, player, flags)
+    local rng = player:GetCardRNG(CARD_GOLDEN)
     if rng:RandomFloat() <= 0.5 then
         if player:HasCollectible(CollectibleType.COLLECTIBLE_BLANK_CARD) == false then
             player:AddCard(CARD_GOLDEN)
         end
     end
-    player:UseCard(pickCard(), UseFlag.USE_NOANIM)
+    player:UseCard(pickCard(rng), UseFlag.USE_NOANIM)
 end
 mod:AddCallback(ModCallbacks.MC_USE_CARD, mod.UseGoldCard, CARD_GOLDEN)
