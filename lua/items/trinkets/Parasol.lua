20b1ccd4d04561333b5260cd93fb3ce88d8745e8
goldenstein64 <goldnstein64@gmail.com>
2022-08-04 12:32:51 -0400

    Fixed weird Forgotten/Parasol bug

--- lua/items/trinkets/Parasol.lua
+++ lua/items/trinkets/Parasol.lua
@@ -1,8 +1,9 @@
 local mod = Furtherance
 
 function mod:BlockShots(entity)
+    if entity.SpawnerEntity == nil then return end
     local player = entity.SpawnerEntity:ToPlayer()
-    if player:HasTrinket(TrinketType.TRINKET_PARASOL) then
+    if player ~= nil and player:HasTrinket(TrinketType.TRINKET_PARASOL) then
         for _, tear in ipairs(Isaac.FindInRadius(entity.Position, 12, EntityPartition.BULLET)) do
             tear:Die()
         end
