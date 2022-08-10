ca5d9186a0b992f8766c841347e65f0e942ec9ee
goldenstein64 <goldnstein64@gmail.com>
2022-08-10 04:42:59 -0400

    Fixed tech -1 going crazy with cricket's body

--- lua/items/collectibles/Technology-1.lua
+++ lua/items/collectibles/Technology-1.lua
@@ -1,13 +1,20 @@
 local mod = Furtherance
 
+---@param tear EntityTear
 function mod:Minus1Shots(tear)
 	local player = tear.SpawnerEntity and tear.SpawnerEntity:ToPlayer()
 	if player and player:HasCollectible(CollectibleType.COLLECTIBLE_TECHNOLOGY_MINUS_1) then
 		local rng = player:GetCollectibleRNG(CollectibleType.COLLECTIBLE_TECHNOLOGY_MINUS_1)
 		if rng:RandomFloat() <= 0.0314 then
 			for i = 1, 3 do
-				player:FireTechLaser(tear.Position, LaserOffset.LASER_TECH1_OFFSET, Vector(math.random(-1, 1), math.random(-1, 1)), true, false, player, 1)
+				local direction = rng:RandomInt(8) * 45
+
+				-- 2 is technology laser
+				local laser = EntityLaser.ShootAngle(2, tear.Position, direction, 3, Vector.Zero, player)
+				laser.TearFlags = player.TearFlags & ~TearFlags.TEAR_QUADSPLIT
+				laser:SetOneHit(true)
 			end
+			tear:ClearTearFlags(TearFlags.TEAR_QUADSPLIT)
 			tear:Die()
 		end
 		tear.Color = Color(0.75, 0, 0, 1, 0.25, 0, 0)
