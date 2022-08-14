73b7d86576dd119a4b65c0e69a1e6f3703f776e1
goldenstein64 <goldnstein64@gmail.com>
2022-08-14 02:26:52 -0400

    golden cards don't replace guaranteed fool card

--- lua/pocket/GoldenCard.lua
+++ lua/pocket/GoldenCard.lua
@@ -1,37 +1,128 @@
 local mod = Furtherance
+local game = Game()
 
-local function isCard(num)
-    return (num < 32 or num > 41) and num ~= 55 and num ~= 78 and num ~= 80
-        and num ~= RUNE_SOUL_OF_MIRIAM and num ~= RUNE_SOUL_OF_LEAH and num ~= RUNE_SOUL_OF_PETER
-        and num ~= RUNE_ESSENCE_OF_LOVE and num ~= RUNE_ESSENCE_OF_HATE
-        and num ~= RUNE_ESSENCE_OF_LIFE and num ~= RUNE_ESSENCE_OF_DEATH
-        and num ~= RUNE_ESSENCE_OF_PROSPERITY and num ~= RUNE_ESSENCE_OF_DROUGHT
-        and num ~= RUNE_ESSENCE_OF_BRAVERY and num ~= RUNE_ESSENCE_OF_COWARDICE
+local allCards = {
+	Card.CARD_FOOL,
+	Card.CARD_MAGICIAN,
+	Card.CARD_HIGH_PRIESTESS,
+	Card.CARD_EMPRESS,
+	Card.CARD_EMPEROR,
+	Card.CARD_HIEROPHANT,
+	Card.CARD_LOVERS,
+	Card.CARD_CHARIOT,
+	Card.CARD_JUSTICE,
+	Card.CARD_HERMIT,
+	Card.CARD_WHEEL_OF_FORTUNE,
+	Card.CARD_STRENGTH,
+	Card.CARD_HANGED_MAN,
+	Card.CARD_DEATH,
+	Card.CARD_TEMPERANCE,
+	Card.CARD_DEVIL,
+	Card.CARD_TOWER,
+	Card.CARD_STARS,
+	Card.CARD_MOON,
+	Card.CARD_SUN,
+	Card.CARD_JUDGEMENT,
+	Card.CARD_WORLD,
+	Card.CARD_CLUBS_2,
+	Card.CARD_DIAMONDS_2,
+	Card.CARD_SPADES_2,
+	Card.CARD_HEARTS_2,
+	Card.CARD_ACE_OF_CLUBS,
+	Card.CARD_ACE_OF_DIAMONDS,
+	Card.CARD_ACE_OF_SPADES,
+	Card.CARD_ACE_OF_HEARTS,
+	Card.CARD_JOKER,
+	Card.CARD_CHAOS,
+	Card.CARD_CREDIT,
+	Card.CARD_RULES,
+	Card.CARD_HUMANITY,
+	Card.CARD_SUICIDE_KING,
+	Card.CARD_GET_OUT_OF_JAIL,
+	Card.CARD_QUESTIONMARK,
+	Card.CARD_EMERGENCY_CONTACT,
+	Card.CARD_HOLY,
+	Card.CARD_HUGE_GROWTH,
+	Card.CARD_ANCIENT_RECALL,
+	Card.CARD_ERA_WALK,
+	Card.CARD_REVERSE_FOOL,
+	Card.CARD_REVERSE_MAGICIAN,
+	Card.CARD_REVERSE_HIGH_PRIESTESS,
+	Card.CARD_REVERSE_EMPRESS,
+	Card.CARD_REVERSE_EMPEROR,
+	Card.CARD_REVERSE_HIEROPHANT,
+	Card.CARD_REVERSE_LOVERS,
+	Card.CARD_REVERSE_CHARIOT,
+	Card.CARD_REVERSE_JUSTICE,
+	Card.CARD_REVERSE_HERMIT,
+	Card.CARD_REVERSE_WHEEL_OF_FORTUNE,
+	Card.CARD_REVERSE_STRENGTH,
+	Card.CARD_REVERSE_HANGED_MAN,
+	Card.CARD_REVERSE_DEATH,
+	Card.CARD_REVERSE_TEMPERANCE,
+	Card.CARD_REVERSE_DEVIL,
+	Card.CARD_REVERSE_TOWER,
+	Card.CARD_REVERSE_STARS,
+	Card.CARD_REVERSE_MOON,
+	Card.CARD_REVERSE_SUN,
+	Card.CARD_REVERSE_JUDGEMENT,
+	Card.CARD_REVERSE_WORLD,
+	Card.CARD_QUEEN_OF_HEARTS,
+	Card.CARD_WILD,
 
-end
+	-- Furtherance Cards
+	CARD_TWO_OF_SHIELDS,
+	CARD_ACE_OF_SHIELDS,
+	CARD_TRAP,
+	CARD_KEY,
+	CARD_GOLDEN,
+	CARD_HOPE,
+	CARD_REVERSE_HOPE,
+	CARD_FAITH,
+	CARD_REVERSE_FAITH,
+	CARD_CHARITY,
+	CARD_REVERSE_CHARITY,
+}
 
-local allCards = {}
-for i = 1, Isaac.GetItemConfig():GetCards().Size do
-    if isCard(i) then
-        table.insert(allCards, i)
-    end
+local allCardsSet = {}
+for _, card in ipairs(allCardsSet) do
+    allCardsSet[card] = true
 end
 
 local function pickCard(rng)
     return allCards[rng:RandomInt(#allCards) + 1]
 end
 
-function mod:SpawnGoldenCard(entityType, variant, subtype, _, _, spawner, seed)
-    if entityType == EntityType.ENTITY_PICKUP and variant == PickupVariant.PICKUP_TAROTCARD and isCard(subtype)
-    and (spawner == nil or spawner.Type ~= EntityType.ENTITY_PLAYER) then
-        local rng = RNG()
-        rng:SetSeed(seed, 1)
-        if rng:RandomFloat() <= 0.025 then
-            return { entityType, variant, CARD_GOLDEN, seed }
-        end
+local processedCards = {}
+
+local skullGridIndex = nil
+function mod:SpawnGoldenCard(entityType, variant, subtype, position, _, spawner, seed)
+    if processedCards[seed] then return end
+    -- has to be a card that is a tarot card
+    if entityType ~= EntityType.ENTITY_PICKUP or variant ~= PickupVariant.PICKUP_TAROTCARD or not allCardsSet[subtype] then return end
+
+    -- spawner cannot be the player
+    if spawner ~= nil and spawner.Type == EntityType.ENTITY_PLAYER then return end
+
+    local room = game:GetRoom()
+    local index = room:GetGridIndex(position)
+    if index == skullGridIndex then return end
+
+    local rng = RNG()
+    rng:SetSeed(seed, 1)
+    if rng:RandomFloat() <= 0.025 then
+        processedCards[seed] = true
+        return { entityType, variant, CARD_GOLDEN, seed }
     end
 end
 mod:AddCallback(ModCallbacks.MC_PRE_ENTITY_SPAWN, mod.SpawnGoldenCard)
+
+function mod:ClearProcessedCards()
+    for k in pairs(processedCards) do
+        processedCards[k] = nil
+    end
+end
+mod:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, mod.ClearProcessedCards)
 
 function mod:UseGoldCard(card, player, flags)
     local rng = player:GetCardRNG(CARD_GOLDEN)
@@ -43,3 +134,17 @@ function mod:UseGoldCard(card, player, flags)
     player:UseCard(pickCard(rng), UseFlag.USE_NOANIM)
 end
 mod:AddCallback(ModCallbacks.MC_USE_CARD, mod.UseGoldCard, CARD_GOLDEN)
+
+function mod:CheckForFoolCard()
+    -- only check in depths II
+    if game:GetLevel():GetStage() ~= LevelStage.STAGE3_2 then return end
+
+    local room = Game():GetRoom()
+    for i = 1, room:GetGridSize() do
+        local gridEntity = room:GetGridEntity(i)
+        if gridEntity ~= nil and gridEntity:GetType() == GridEntityType.GRID_ROCK_ALT2 then
+            skullGridIndex = i
+        end
+    end
+end
+mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, mod.CheckForFoolCard)
