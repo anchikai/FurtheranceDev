f4fcf2ee5d8d0686a5f5498d793bb66ae492fcb6
anchikai <hentaisuperstar86@gmail.com>
2022-08-18 22:10:39 -0400

    Sunscreen isnt fucking useless now

--- lua/items/collectibles/Sunscreen.lua
+++ lua/items/collectibles/Sunscreen.lua
@@ -2,8 +2,7 @@ local mod = Furtherance
 
 function mod:SunscreenDamage(entity, amount, flag)
     local player = entity:ToPlayer()
-    local rng = player:GetCollectibleRNG(CollectibleType.COLLECTIBLE_SUNSCREEN)
-    if rng:RandomFloat() <= 0.25 and player:HasCollectible(CollectibleType.COLLECTIBLE_SUNSCREEN) and flag & DamageFlag.DAMAGE_FIRE == DamageFlag.DAMAGE_FIRE then
+    if player:HasCollectible(CollectibleType.COLLECTIBLE_SUNSCREEN) and flag & DamageFlag.DAMAGE_FIRE == DamageFlag.DAMAGE_FIRE then
         player:ResetDamageCooldown()
         player:SetMinDamageCooldown(60)
         return false
