4e47be582c2ba687248c1d9a5b128589f8156f89
goldenstein64 <goldnstein64@gmail.com>
2022-08-31 19:51:39 -0400

    fixed game die

--- lua/items/collectibles/GameDie.lua
+++ lua/items/collectibles/GameDie.lua
@@ -2,23 +2,28 @@ local mod = Furtherance
 
 function mod:UseGameDie(_, _, player)
     local itemConfig = Isaac.GetItemConfig()
-	for _, entity in ipairs(Isaac.FindByType(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE)) do
-        if entity.SubType ~= 0 then
-            local OldItemQuality = itemConfig:GetCollectible(entity.SubType).Quality
-            for i = 1, 2 do
-                local NewItems = Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, 0, Isaac.GetFreeNearPosition(entity.Position, 40), Vector.Zero, player):ToPickup()         
-                while (itemConfig:GetCollectible(NewItems.SubType).Quality >= OldItemQuality) do
-                    if itemConfig:GetCollectible(NewItems.SubType).Quality < 1 then
-                        NewItems:Remove()
-                    else
-                        print("Quality " .. itemConfig:GetCollectible(NewItems.SubType).Quality .. ", rerolling!")
-                        NewItems:Morph(entity.Type, entity.Variant, 0, false, false, false)
+	for i, item in ipairs(Isaac.FindByType(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE)) do
+        if item.SubType ~= 0 then
+            -- if the item pool runs out, items morph to breakfast by default.
+            -- so we can't reroll items that are already breakfast, it'll just
+            -- create more of them.
+            local oldItemQuality = itemConfig:GetCollectible(item.SubType).Quality
+            if item.SubType ~= CollectibleType.COLLECTIBLE_BREAKFAST and oldItemQuality > 0 then
+                for _ = 1, 2 do
+                    local newItem = Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, 0, Isaac.GetFreeNearPosition(item.Position, 40), Vector.Zero, player):ToPickup()
+
+                    local maxItemQuality = math.max(0, oldItemQuality - 1)
+                    while newItem.SubType ~= CollectibleType.COLLECTIBLE_BREAKFAST and itemConfig:GetCollectible(newItem.SubType).Quality > maxItemQuality do
+                        newItem:Morph(item.Type, item.Variant, 0, false, false, false)
                     end
+
+                    newItem.Price = item:ToPickup().Price
+                    newItem.OptionsPickupIndex = 123 + i
                 end
-                NewItems.Price = entity:ToPickup().Price
-                NewItems.OptionsPickupIndex = 123
             end
-            entity:Remove()
+
+            item:Remove()
+            Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, 0, item.Position, Vector.Zero, item)
         end
     end
 	return true
